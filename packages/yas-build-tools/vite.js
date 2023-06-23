ec5913bbff29d1cacb5c30cd86d32a2e3a4d872d
ksandin <kasper.sandin@gmail.com>
2023-06-23 10:01:51 +0200

    Remove build-time env validation
    It was too complicated

--- packages/yas-build-tools/vite.js
+++ packages/yas-build-tools/vite.js
@@ -1,29 +1,23 @@
 const path = require("path");
 const react = require("@vitejs/plugin-react");
-const { default: env } = require("vite-plugin-environment");
 const { visualizer } = require("rollup-plugin-visualizer");
-const { expand } = require("dotenv-expand");
 const { defineConfig } = require("vite");
 const { default: checker } = require("vite-plugin-checker");
-const { config } = require("dotenv-flow");
+const { expand } = require("dotenv-expand");
 
 // NOTE This is a JS file because it was much simpler than configuring
 // Vite & node to load non-prebuilt ESM files via the workspace:* monorepo directive.
 
 function createYasViteConfig({ analyze = process.env.ANALYZE } = {}) {
-  expand(
-    config({
-      path: path.resolve(__dirname, "../.."),
-      default_node_env: "development",
-    })
-  );
+  expand({ parsed: process.env });
 
   return defineConfig({
+    envPrefix: "PUBLIC_",
+    envDir: path.resolve(__dirname, "../../"),
     plugins: [
       react(),
       checker({ typescript: true }),
       determineVisualizerPlugin(analyze),
-      env("all"),
     ].filter(Boolean),
   });
 }
