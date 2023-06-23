64e770919de5a7e3a21f8054dec68da1e3989074
ksandin <kasper.sandin@gmail.com>
2023-06-23 05:37:27 +0200

    Don't output to /api

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  outDir: "api", // Output to Vercel's /api folder
   outExtension: () => ({ js: `.js` }),
   entryPoints: {
     index: "src/entrypoints/prod.ts",
