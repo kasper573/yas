e7001945b98315263844670da3dc812bbe50dcb9
ksandin <kasper.sandin@gmail.com>
2023-06-23 09:51:47 +0200

    Stop using deprecated option

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   outExtension: () => ({ js: `.js` }),
-  entryPoints: { index: "src/index.ts" },
+  entry: { index: "src/index.ts" },
   format: "esm",
   clean: true,
   noExternal: [/^@yas\//],
