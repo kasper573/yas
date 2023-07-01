a6784c87bd7bae8390bf647fb5ecb1374a9386ec
ksandin <kasper.sandin@gmail.com>
2023-07-01 02:58:03 +0200

    Phrasing

--- packages/react-imperative-hook/src/createImperative.test.tsx
+++ packages/react-imperative-hook/src/createImperative.test.tsx
@@ -9,62 +9,66 @@ import { createImperative } from "./createImperative";
 import type { ImperativeComponentProps } from "./index";
 import { ComponentStore } from "./index";
 
-test("predefined dialog with built-in message", async () => {
-  setup(() => {
-    const alert = useModal(Dialog);
-    return <button onClick={() => alert()}>Open dialog</button>;
+describe("can display", () => {
+  test("predefined dialog with built-in message", async () => {
+    setup(() => {
+      const alert = useModal(Dialog);
+      return <button onClick={() => alert()}>Open dialog</button>;
+    });
+    userEvent.click(screen.getByText("Open dialog"));
+    const dialog = await screen.findByRole("dialog");
+    await within(dialog).findByText("Built-in message");
   });
-  userEvent.click(screen.getByText("Open dialog"));
-  const dialog = await screen.findByRole("dialog");
-  await within(dialog).findByText("Built-in message");
-});
 
-test("predefined dialog with default message", async () => {
-  setup(() => {
-    const alert = useModal(Dialog, { message: "Default message" });
-    return <button onClick={() => alert()}>Open dialog</button>;
+  test("predefined dialog with default message", async () => {
+    setup(() => {
+      const alert = useModal(Dialog, { message: "Default message" });
+      return <button onClick={() => alert()}>Open dialog</button>;
+    });
+    userEvent.click(screen.getByText("Open dialog"));
+    const dialog = await screen.findByRole("dialog");
+    await within(dialog).findByText("Default message");
   });
-  userEvent.click(screen.getByText("Open dialog"));
-  const dialog = await screen.findByRole("dialog");
-  await within(dialog).findByText("Default message");
-});
 
-test("predefined dialog with custom message", async () => {
-  setup(() => {
-    const alert = useModal(Dialog, { message: "Default message" });
-    return (
-      <button onClick={() => alert({ message: "Custom message" })}>
-        Open dialog
-      </button>
-    );
+  test("predefined dialog with custom message", async () => {
+    setup(() => {
+      const alert = useModal(Dialog, { message: "Default message" });
+      return (
+        <button onClick={() => alert({ message: "Custom message" })}>
+          Open dialog
+        </button>
+      );
+    });
+    userEvent.click(screen.getByText("Open dialog"));
+    const dialog = await screen.findByRole("dialog");
+    await within(dialog).findByText("Custom message");
   });
-  userEvent.click(screen.getByText("Open dialog"));
-  const dialog = await screen.findByRole("dialog");
-  await within(dialog).findByText("Custom message");
-});
 
-test("inline dialog with built-in message", async () => {
-  setup(() => {
-    const showModal = useModals();
-    return <button onClick={() => showModal(Dialog)}>Open dialog</button>;
+  test("inline dialog with built-in message", async () => {
+    setup(() => {
+      const showModal = useModals();
+      return <button onClick={() => showModal(Dialog)}>Open dialog</button>;
+    });
+    userEvent.click(screen.getByText("Open dialog"));
+    const dialog = await screen.findByRole("dialog");
+    await within(dialog).findByText("Built-in message");
   });
-  userEvent.click(screen.getByText("Open dialog"));
-  const dialog = await screen.findByRole("dialog");
-  await within(dialog).findByText("Built-in message");
-});
 
-test("inline dialog with custom message", async () => {
-  setup(() => {
-    const showModal = useModals();
-    return (
-      <button onClick={() => showModal(Dialog, { message: "Custom message" })}>
-        Open dialog
-      </button>
-    );
+  test("inline dialog with custom message", async () => {
+    setup(() => {
+      const showModal = useModals();
+      return (
+        <button
+          onClick={() => showModal(Dialog, { message: "Custom message" })}
+        >
+          Open dialog
+        </button>
+      );
+    });
+    userEvent.click(screen.getByText("Open dialog"));
+    const dialog = await screen.findByRole("dialog");
+    await within(dialog).findByText("Custom message");
   });
-  userEvent.click(screen.getByText("Open dialog"));
-  const dialog = await screen.findByRole("dialog");
-  await within(dialog).findByText("Custom message");
 });
 
 function Dialog({
