99815d6d8c41aa54647a9f5945ad0f2ef1320943
ksandin <kasper.sandin@gmail.com>
2023-07-02 07:23:07 +0200

    Make publicize-package-json a separate package

--- packages/publicize-package-json/bin/publicize-package-json.ts
+++ packages/publicize-package-json/bin/publicize-package-json.ts
@@ -2,14 +2,21 @@
 
 import * as path from "path";
 import { execa } from "execa";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { createMutableResource } from "./createMutableResource";
 
 /**
- * Wrapper around semantic-release.
- * Strips internal package details from package.json before running semantic-release.
- * Restores package.json after semantic-release is done, but keeps the new version intact.
+ * Strips internal package details from package.json before running a command.
+ * Restores package.json after the command is done, but keeps the new version intact.
  */
-async function run(distFolder: string = process.argv[2] ?? "dist") {
+async function run({
+  command,
+  distFolder,
+}: {
+  command: string;
+  distFolder: string;
+}) {
   const packageFolder = path.resolve(distFolder, "..");
   const packageJsonPath = path.resolve(packageFolder, "package.json");
   const relativeDistFolder = path.relative(packageFolder, distFolder);
@@ -21,11 +28,28 @@ async function run(distFolder: string = process.argv[2] ?? "dist") {
   );
 
   pkg.update((pkg) => makePackageReleaseReady(pkg, relativeDistFolder));
+  console.log("package.json has been made release ready");
 
-  await execa("semantic-release");
-  pkg.reload();
-
-  pkg.restore((original) => (original.version = pkg.contents.version));
+  try {
+    console.log(`Running command "${command}"`);
+    await execa(command);
+  } catch (e) {
+    console.error(
+      `Error while running command "${command}":\n` + (e as Error).message
+    );
+  } finally {
+    pkg.reload();
+    pkg.restore((original) => {
+      if (original.version !== pkg.contents.version) {
+        original.version = pkg.contents.version;
+        console.log(
+          "Retaining new package version before restoring original package.json: " +
+            pkg.contents.version
+        );
+      }
+    });
+    console.log("package.json has been restored");
+  }
 }
 
 function makePackageReleaseReady(pkg: PackageJson, distFolder: string) {
@@ -60,7 +84,14 @@ function removeInternalPackageDependencies(deps: Record<string, string> = {}) {
   }
 }
 
-run();
+const args = yargs(hideBin(process.argv))
+  .options({
+    command: { type: "string", alias: "c", demandOption: true },
+    distFolder: { type: "string", alias: "d", demandOption: true },
+  })
+  .parseSync();
+
+run(args);
 
 type PackageJson = {
   version: string;
