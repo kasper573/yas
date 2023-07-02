9c7a82791498daf67420813859d6fa9677676025
ksandin <kasper.sandin@gmail.com>
2023-07-02 07:32:57 +0200

    Simplify yas-release package

--- packages/yas-release/release.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env tsx
-
-// yas-release is just a proxy of semantic-release.
-// The package exists to centralize configuration and dependencies.
-require("semantic-release/bin/semantic-release");
