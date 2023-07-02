1b7853387572f2a04dc9c9ed6f0ddc4c669def99
ksandin <kasper.sandin@gmail.com>
2023-07-02 07:31:06 +0200

    Make a yas-release package

---
+++ packages/yas-release/release.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env tsx
+
+// yas-release is just a proxy of semantic-release.
+// The package exists to centralize configuration and dependencies.
+require("semantic-release/bin/semantic-release");
