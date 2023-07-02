e1f05e56adb3c8303ac1407b43c8077a92c3ec22
ksandin <kasper.sandin@gmail.com>
2023-07-02 12:10:09 +0200

    Some property docs

--- packages/react-imperative-hook/src/ComponentOutlet.tsx
+++ packages/react-imperative-hook/src/ComponentOutlet.tsx
@@ -3,17 +3,28 @@ import { createElement, useContext, useEffect, useReducer } from "react";
 import type { ComponentStore, ComponentStoreState } from "./ComponentStore";
 import { defaultOutletRenderer } from "./defaultOutletRenderer";
 
+export interface ComponentOutletProps {
+  /**
+   * The context that holds the ComponentStore that contains the components that should be output.
+   */
+  context: Context<ComponentStore>;
+  /**
+   * The OutletRenderer to use. Defaults to defaultOutletRenderer.
+   */
+  renderer?: OutletRenderer;
+}
+
 export function ComponentOutlet({
   context,
   renderer = defaultOutletRenderer,
-}: {
-  context: Context<ComponentStore>;
-  renderer?: OutletRenderer;
-}) {
+}: ComponentOutletProps) {
   const store = useContext(context);
   const [, rerender] = useReducer((x) => x + 1, 0);
   useEffect(() => store.subscribe(rerender), [store]);
   return createElement(renderer, store.state);
 }
 
+/**
+ * A React component that renders the currently registered components and instances in the ComponentStore.
+ */
 export type OutletRenderer = ComponentType<ComponentStoreState>;
