c7225ccd5bc80e25e8b1ba95abbb7e72405f50ff
ksandin <kasper.sandin@gmail.com>
2023-07-11 10:18:29 +0200

    Less eslint-disables

--- packages/react-composable-form/.eslintrc.js
+++ packages/react-composable-form/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: ["yas"],
+  rules: {
+    // Due to frequent use of generics in this library we need to disable these rules
+    "@typescript-eslint/ban-types": ["error", { types: { "{}": false } }],
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 };
