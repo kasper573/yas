b66842e003fb40e61c372884ca41f8301d2931bc
ksandin <kasper.sandin@gmail.com>
2023-07-11 10:27:08 +0200

    Less eslint-disables

--- packages/react-imperative-hook/.eslintrc.js
+++ packages/react-imperative-hook/.eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   extends: ["yas"],
   rules: {
     // Due to frequent use of generics in this library we need to disable these rules
-    "@typescript-eslint/ban-types": ["error", { types: { "{}": false } }],
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      { types: { "{}": false }, extendDefaults: true },
+    ],
   },
 };
