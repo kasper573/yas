71ee4ab545117b333727c3103e8824de37a2cffa
ksandin <kasper.sandin@gmail.com>
2023-07-22 02:26:46 +0200

    Remove unused types

--- packages/react-composable-form/src/types/utilityTypes.ts
+++ packages/react-composable-form/src/types/utilityTypes.ts
@@ -5,10 +5,6 @@ export type AnyProps = Record<string, any>;
 
 export type AnyComponent = ComponentType<any>;
 
-export type Fallback<A, B> = IsNever<A> extends true ? B : A;
-
-export type IsNever<T> = [T] extends [never] ? true : false;
-
 export type TypeNameForType<Type extends ZodType> =
   Type["_def"] extends ZodDefinitionWithTypeName
     ? Type["_def"]["typeName"]
