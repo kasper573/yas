639cecec1f12ef98b7f6fe4ad6bf4660c0cf903e
ksandin <kasper.sandin@gmail.com>
2023-07-25 23:35:51 +0200

    Support password text fields

--- apps/rcf-examples/src/fields/TextField.tsx
+++ apps/rcf-examples/src/fields/TextField.tsx
@@ -1,13 +1,18 @@
 import type { FieldProps } from "react-composable-form";
 import { TextField as MuiTextField } from "@mui/material";
 
+export interface TextFieldProps extends FieldProps<string | undefined> {
+  password?: boolean;
+}
+
 export function TextField({
   name,
   value,
   errors = [],
   onChange,
-  onBlur,
-}: FieldProps<string | undefined>) {
+  password,
+  ...rest
+}: TextFieldProps) {
   return (
     <MuiTextField
       value={value ?? ""}
@@ -15,7 +20,8 @@ export function TextField({
       error={errors.length > 0}
       helperText={errors.join(", ")}
       onChange={(e) => onChange(e.target.value || undefined)}
-      onBlur={onBlur}
+      type={password ? "password" : "text"}
+      {...rest}
     />
   );
 }
