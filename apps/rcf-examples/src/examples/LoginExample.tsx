136b1194bff6cf8ec37f52bce59757ff7a2fbe74
ksandin <kasper.sandin@gmail.com>
2023-07-25 23:03:53 +0200

    Make example header part of the default form layout

--- apps/rcf-examples/src/examples/LoginExample.tsx
+++ apps/rcf-examples/src/examples/LoginExample.tsx
@@ -1,44 +1,22 @@
 import { z } from "zod";
 import { useState } from "react";
 import type { inferFormValue } from "react-composable-form";
-import { Button } from "@mui/material";
 import { BaseForm } from "../forms/BaseForm";
-import { SingleSelectField, valueOptions } from "../fields/SelectField";
-import { ExampleHeader } from "../components/ExampleHeader";
-
-const categoryType = z.enum(["foo", "bar", "baz"]);
-const category2Type = z.enum(["hello", "world"]);
 
 const UserForm = BaseForm.extend((options) =>
-  options
-    .schema(
-      z.object({
-        firstName: z.string(),
-        lastName: z.string(),
-        email: z.string().email(),
-        age: z.number(),
-        category: categoryType,
-        category2: category2Type,
-      }),
-    )
-    .type(categoryType, SingleSelectField, {
-      options: valueOptions(categoryType.options),
-    })
-    .type(category2Type, SingleSelectField, {
-      options: valueOptions(category2Type.options),
+  options.schema(
+    z.object({
+      email: z.string().email(),
+      password: z.string(),
     }),
+  ),
 );
 
 export function LoginExample() {
   const [data, setData] = useState<inferFormValue<typeof UserForm>>();
   return (
     <>
-      <ExampleHeader title="Login">
-        <Button variant="outlined">Reset</Button>
-        <Button variant="contained">Submit</Button>
-      </ExampleHeader>
-      <UserForm value={data} onChange={setData} />
-      <pre>{JSON.stringify({ data }, null, 2)}</pre>
+      <UserForm value={data} onChange={setData} title="Login form" />
     </>
   );
 }
--- apps/rcf-examples/src/layouts/DefaultFormLayout.tsx
+++ apps/rcf-examples/src/layouts/DefaultFormLayout.tsx
@@ -1,15 +1,28 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import type { FormLayoutProps } from "react-composable-form";
+import type { ReactNode } from "react";
 
-export function DefaultFormLayout({ fields, handleSubmit }: FormLayoutProps) {
+export function DefaultFormLayout({
+  title,
+  fields,
+  handleSubmit,
+}: FormLayoutProps & { title: ReactNode }) {
   return (
     <form onSubmit={handleSubmit}>
+      <Stack direction="row" sx={{ justifyContent: "space-between", mb: 3 }}>
+        <Typography variant="h4">{title}</Typography>
+        <Stack direction="row" gap={2}>
+          <Button variant="outlined">Reset</Button>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
       <Stack direction="column" gap={2}>
         {Object.values(fields).map((Component, index) => (
           <Component key={index} />
         ))}
       </Stack>
-      <Button type="submit">Submit</Button>
     </form>
   );
 }
