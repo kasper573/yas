b3a1990286838d6717719461e0addfa66d9a44df
ksandin <kasper.sandin@gmail.com>
2023-07-25 23:36:09 +0200

    Configure the password field to be a password text field

--- apps/rcf-examples/src/examples/LoginExample.tsx
+++ apps/rcf-examples/src/examples/LoginExample.tsx
@@ -2,21 +2,20 @@ import { z } from "zod";
 import { useState } from "react";
 import type { inferFormValue } from "react-composable-form";
 import { BaseForm } from "../forms/BaseForm";
+import { TextField } from "../fields/TextField";
 
 const UserForm = BaseForm.extend((options) =>
-  options.schema(
-    z.object({
-      email: z.string().email(),
-      password: z.string(),
-    }),
-  ),
+  options
+    .schema(
+      z.object({
+        email: z.string().email(),
+        password: z.string(),
+      }),
+    )
+    .field("password", TextField, { password: true }),
 );
 
 export function LoginExample() {
   const [data, setData] = useState<inferFormValue<typeof UserForm>>();
-  return (
-    <>
-      <UserForm value={data} onChange={setData} title="Login form" />
-    </>
-  );
+  return <UserForm value={data} onChange={setData} title="Login form" />;
 }
