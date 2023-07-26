0bc917436c2edf0475a3e6f14f03bb76cf594fb7
ksandin <kasper.sandin@gmail.com>
2023-07-26 23:45:14 +0200

    Add placeholder for data format example

--- apps/rcf-examples/src/examples/index.ts
+++ apps/rcf-examples/src/examples/index.ts
@@ -3,9 +3,11 @@ import { ConditionalExample } from "./ConditionalExample";
 import { ListExample } from "./ListExample";
 import { NestedExample } from "./NestedExample";
 import { StepsExample } from "./StepsExample";
+import { DataFormatExample } from "./DataFormatExample";
 
 export const examples = [
   { name: "Layout", component: LayoutExample },
+  { name: "DataFormat", component: DataFormatExample },
   { name: "Conditional", component: ConditionalExample },
   { name: "Steps", component: StepsExample },
   { name: "List", component: ListExample },
