c3203d79b8c3cffb58b5fea0dcc93c06e38e7119
ksandin <kasper.sandin@gmail.com>
2023-07-26 23:07:37 +0200

    Add placeholder components for examples I want to create

--- apps/rcf-examples/src/examples/index.ts
+++ apps/rcf-examples/src/examples/index.ts
@@ -1,3 +1,11 @@
 import { LayoutExample } from "./LayoutExample";
+import { ConditionalExample } from "./ConditionalExample";
+import { ListExample } from "./ListExample";
+import { NestedExample } from "./NestedExample";
 
-export const examples = [{ name: "Layout", component: LayoutExample }];
+export const examples = [
+  { name: "Layout", component: LayoutExample },
+  { name: "Conditional", component: ConditionalExample },
+  { name: "List", component: ListExample },
+  { name: "Nested", component: NestedExample },
+];
