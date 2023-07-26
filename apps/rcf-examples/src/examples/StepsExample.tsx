e7e74f644bb3d14c714709d5d5bf8ad248d4e1f4
ksandin <kasper.sandin@gmail.com>
2023-07-26 23:37:43 +0200

    Add placeholder for steps example

---
+++ apps/rcf-examples/src/examples/StepsExample.tsx
@@ -0,0 +1,3 @@
+export function StepsExample() {
+  return null;
+}
--- apps/rcf-examples/src/examples/index.ts
+++ apps/rcf-examples/src/examples/index.ts
@@ -2,10 +2,12 @@ import { LayoutExample } from "./LayoutExample";
 import { ConditionalExample } from "./ConditionalExample";
 import { ListExample } from "./ListExample";
 import { NestedExample } from "./NestedExample";
+import { StepsExample } from "./StepsExample";
 
 export const examples = [
   { name: "Layout", component: LayoutExample },
   { name: "Conditional", component: ConditionalExample },
+  { name: "Steps", component: StepsExample },
   { name: "List", component: ListExample },
   { name: "Nested", component: NestedExample },
 ];
