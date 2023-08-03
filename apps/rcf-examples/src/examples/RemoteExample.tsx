fe87d5e96af4d84a73f7d449edccaeb0033db56e
ksandin <kasper.sandin@gmail.com>
2023-08-03 23:50:52 +0200

    Update remote example to use react-query

--- apps/rcf-examples/src/examples/RemoteExample.tsx
+++ apps/rcf-examples/src/examples/RemoteExample.tsx
@@ -1,58 +1,65 @@
 import { z } from "zod";
 import type { inferFormValue } from "react-composable-form";
-import { useCallback, useState } from "react";
+import { QueryClient, QueryClientProvider, useMutation } from "react-query";
+import { useMemo } from "react";
 import { BaseForm } from "../BaseForm";
 
-type User = inferFormValue<typeof UserRegistrationForm>;
+interface CustomRemoteErrors {
+  general?: string[];
+  field?: Array<[string, string[]]>;
+}
+
+type FormData = inferFormValue<typeof UserRegistrationForm>;
 const UserRegistrationForm = BaseForm.extend((options) =>
-  options.schema(
-    z.object({
-      email: z.string().email(),
-      password: z.string().min(6),
-      passwordConfirm: z.string().min(6),
-    }),
-  ),
+  options
+    .externalErrorsNormalizer(
+      ({ general, field = [] }: CustomRemoteErrors) => ({
+        general,
+        field: Object.fromEntries(field),
+      }),
+    )
+    .schema(
+      z.object({
+        email: z.string().email(),
+        password: z.string().min(6),
+        passwordConfirm: z.string().min(6),
+      }),
+    ),
 );
 
-function useMutation() {
-  const [fieldErrors, setFieldErrors] = useState<Record<string, string[]>>({});
-  const [generalErrors, setGeneralErrors] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const submit = useCallback(
-    async (value: User) => {
-      setIsLoading(true);
-      setFieldErrors({});
-      setGeneralErrors([]);
-      await wait(1000); // Emulate server side validation
-      if (value.password !== value.passwordConfirm) {
-        setFieldErrors({
-          password: ["Passwords must match"],
-          passwordConfirm: ["Passwords must match"],
-        });
-      } else {
-        setFieldErrors({});
-      }
-      setGeneralErrors(["You are not allowed to register at this time"]);
-      setIsLoading(false);
-    },
-    [setFieldErrors],
+function RemoteExampleImpl() {
+  const { mutate, error, isLoading } = useMutation<
+    unknown,
+    CustomRemoteErrors,
+    FormData
+  >(simulatedEndpoint);
+  return (
+    <UserRegistrationForm
+      onSubmit={mutate}
+      errors={error}
+      title="asdf"
+      isLoading={isLoading}
+    />
   );
-  return { submit, fieldErrors, generalErrors, isLoading };
 }
 
 export function RemoteExample() {
-  const { submit, fieldErrors, generalErrors, isLoading } = useMutation();
+  const queryClient = useMemo(() => new QueryClient(), []);
   return (
-    <>
-      <UserRegistrationForm
-        onSubmit={submit}
-        generalErrors={generalErrors}
-        fieldErrors={fieldErrors}
-        title="asdf"
-        isLoading={isLoading}
-      />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RemoteExampleImpl />
+    </QueryClientProvider>
   );
 }
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function simulatedEndpoint(value: FormData) {
+  await wait(1000); // Emulate server side validation
+  const errors: CustomRemoteErrors = {};
+  if (value.password !== value.passwordConfirm) {
+    errors.field = [["password", ["Passwords do not match"]]];
+  }
+  errors.general = ["You are not allowed to register at this time"];
+  throw errors;
+}
