7487557b98d4eb0ee4d338793e980bd9de286a6d
ksandin <kasper.sandin@gmail.com>
2023-08-04 15:56:29 +0200

    Update fast refresh test to use testing library

--- packages/react-composable-form/src/__test__/reactFastRefresh.test.tsx
+++ packages/react-composable-form/src/__test__/reactFastRefresh.test.tsx
@@ -3,78 +3,57 @@ import "@testing-library/jest-dom";
 import type { ComponentType } from "react";
 import React from "react";
 import ReactFreshRuntime from "react-refresh/runtime";
-import type { Root } from "react-dom/client";
+import userEvent from "@testing-library/user-event";
 
 // React refresh runtime must inject global hook before
 // importing react-dom/client and react-dom/test-utils
 ReactFreshRuntime.injectIntoGlobalHook(window);
-import ReactDOMClient = require("react-dom/client");
-const { act } = require("react-dom/test-utils");
+import rtl = require("@testing-library/react");
+const { act, render } = rtl;
 
 describe("reactFastRefresh", () => {
-  let container: HTMLDivElement, reactRoot: Root;
-
-  beforeEach(() => {
-    globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-    container = document.createElement("div");
-    document.body.appendChild(container);
-    reactRoot = ReactDOMClient.createRoot(container);
-  });
-
-  afterEach(() => {
-    delete global.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-    document.body.removeChild(container);
-  });
-
-  let idCounter = 0;
-  function prepare(Original: ComponentType) {
-    const id = (idCounter++).toString();
-    act(() => reactRoot.render(<Original />));
-    ReactFreshRuntime.register(Original, id);
-    function patch(Updated: ComponentType) {
-      ReactFreshRuntime.register(Updated, id);
-      act(() => ReactFreshRuntime.performReactRefresh());
-    }
-    return { patch };
-  }
-
-  it("can preserve state for compatible types", () => {
-    const { patch } = prepare(function Hello() {
+  it("can preserve state for compatible types", async () => {
+    const { getByRole, patch } = prepare(function Hello() {
       const [val, setVal] = React.useState(0);
       return (
-        <p style={{ color: "blue" }} onClick={() => setVal(val + 1)}>
+        <button style={{ color: "blue" }} onClick={() => setVal(val + 1)}>
           {val}
-        </p>
+        </button>
       );
     });
 
     // Bump the state before patching.
-    const el = container.firstChild as HTMLParagraphElement;
-    expect(el.textContent).toBe("0");
-    expect(el.style.color).toBe("blue");
-    act(() => {
-      el.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-    });
-    expect(el.textContent).toBe("1");
+    let button = getByRole("button");
+    expect(button).toHaveTextContent("0");
+    expect(button).toHaveStyle({ color: "blue" });
+    await userEvent.click(button);
+    expect(button).toHaveTextContent("1");
 
     // Perform a hot update.
-    patch(function Hello() {
+    await patch(function Hello() {
       const [val, setVal] = React.useState(0);
       return (
-        <p style={{ color: "red" }} onClick={() => setVal(val + 1)}>
+        <button style={{ color: "red" }} onClick={() => setVal(val + 1)}>
           {val}
-        </p>
+        </button>
       );
     });
 
     // Assert the state was preserved but color changed.
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe("1");
-    expect(el.style.color).toBe("red");
+    button = getByRole("button");
+    expect(button).toHaveTextContent("1");
+    expect(button).toHaveStyle({ color: "red" });
   });
 });
 
-declare global {
-  var IS_REACT_ACT_ENVIRONMENT: boolean;
-  var __REACT_DEVTOOLS_GLOBAL_HOOK__: unknown;
+let idCounter = 0;
+function prepare(Original: ComponentType) {
+  const id = (idCounter++).toString();
+  const result = render(<Original />);
+  ReactFreshRuntime.register(Original, id);
+  async function patch(Updated: ComponentType) {
+    ReactFreshRuntime.register(Updated, id);
+    await act(() => ReactFreshRuntime.performReactRefresh());
+  }
+  return { ...result, patch };
 }
