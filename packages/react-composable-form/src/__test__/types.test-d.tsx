dc527358f853fd64ed82e22b16ead2cf90a08623
ksandin <kasper.sandin@gmail.com>
2023-08-05 22:07:56 +0200

    Add test to disallow unknown form properties

--- packages/react-composable-form/src/__test__/types.test-d.tsx
+++ packages/react-composable-form/src/__test__/types.test-d.tsx
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { expectTypeOf } from "vitest";
+import type { ComponentType } from "react";
 import type { inferFormValue } from "../createForm";
 import { createForm } from "../createForm";
 
@@ -18,4 +19,12 @@ it("Form inherits its layouts properties", () => {
   type Props = { foo: string };
   const Form = createForm((options) => options.layout((props: Props) => null));
   expectTypeOf(Form).parameter(0).toMatchTypeOf<Props>();
+});
+
+it("Form does not accept unknown properties", () => {
+  const Form = createForm();
+  expectTypeOf(Form).toMatchTypeOf<ComponentType>();
+
+  // @ts-expect-error Should not accept unknown properties
+  const res = <Form unknownProp={123} />;
 });
