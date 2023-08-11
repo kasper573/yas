68450c5208bffc2a0f2519b742c4a6a8c4ff5a50
ksandin <kasper.sandin@gmail.com>
2023-08-11 19:12:42 +0200

    Update conditional example

--- apps/rcf-examples/src/examples/ConditionalExample.tsx
+++ apps/rcf-examples/src/examples/ConditionalExample.tsx
@@ -3,19 +3,30 @@ import { BaseForm } from "../BaseForm";
 import { ExampleContent } from "../ExampleContent";
 import { SingleSelectField, valueOptions } from "../fields/SingleSelectField";
 
-const paymentType = z.enum(["PayPal", "Card"]);
+const cardSchema = z.object({
+  type: z.literal("card"),
+  number: z.string(),
+  expiry: z.string(),
+});
+
+const paypalSchema = z.object({
+  type: z.literal("paypal"),
+  email: z.string(),
+});
+
+const paymentType = z.enum([
+  cardSchema.shape.type.value,
+  paypalSchema.shape.type.value,
+]);
+
+const paymentSchema = z
+  .object({ owner: z.string() })
+  .and(z.discriminatedUnion("type", [cardSchema, paypalSchema]));
 
 const PaymentForm = BaseForm.extend((options) =>
-  options
-    .schema(
-      z.object({
-        owner: z.string(),
-        paymentType,
-      }),
-    )
-    .type(paymentType, SingleSelectField, {
-      options: valueOptions(paymentType.options),
-    }),
+  options.schema(paymentSchema).type(paymentType, SingleSelectField, {
+    options: valueOptions(paymentType.options),
+  }),
 );
 
 export function ConditionalExample() {
