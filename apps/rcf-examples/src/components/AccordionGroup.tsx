16a63c020dbab58f7d281a89daafea52159b0d44
ksandin <kasper.sandin@gmail.com>
2023-08-13 14:33:43 +0200

    Create AccordionGroup component

---
+++ apps/rcf-examples/src/components/AccordionGroup.tsx
@@ -0,0 +1,33 @@
+import type { ComponentProps, ComponentType, ReactNode } from "react";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+
+export interface AccordionByGroupsProps
+  extends Omit<ComponentProps<typeof Accordion>, "children"> {
+  entries?: Record<string, ReactNode>;
+  components?: {
+    Summary?: ComponentType<{ children?: ReactNode }>;
+  };
+}
+
+export function AccordionGroup({
+  entries = {},
+  components: { Summary = PassThrough } = {},
+  ...accordionProps
+}: AccordionByGroupsProps) {
+  return (
+    <>
+      {Object.entries(entries).flatMap(([title, content]) => (
+        <Accordion key={title} {...accordionProps}>
+          <AccordionSummary>
+            <Summary>{title}</Summary>
+          </AccordionSummary>
+          <AccordionDetails>{content}</AccordionDetails>
+        </Accordion>
+      ))}
+    </>
+  );
+}
+
+function PassThrough({ children }: { children?: ReactNode }) {
+  return <>{children}</>;
+}
