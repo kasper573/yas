954a024dab438b985f5cbd6b5d2857abba9662f9
ksandin <kasper.sandin@gmail.com>
2023-08-20 23:09:45 +0200

    Handle strict mode in useDeferredFieldValues.ts

--- packages/react-composable-form/src/useDeferredFieldValues.ts
+++ packages/react-composable-form/src/useDeferredFieldValues.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
+import { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import type { FieldNames, FormSchema, inferValue } from "./types/commonTypes";
 import type { FormStore } from "./FormStore";
 
@@ -9,50 +9,69 @@ export function useDeferredFieldValues<Schema extends FormSchema>(
   allFieldNames: Map<FieldNames<Schema>, boolean>,
   store: FormStore<Schema>,
 ): inferValue<Schema> {
-  const subscriptions = useRef({} as Record<FieldNames<Schema>, () => void>);
   const [, forceRefresh] = useReducer((n) => n + 1, 0);
+  const [touchedFields] = useState(
+    () => new Map<FieldNames<Schema>, (() => void) | undefined>(),
+  );
 
-  const clearSubscriptions = useCallback(() => {
-    for (const key in subscriptions.current) {
-      const fieldName = key as FieldNames<Schema>;
-      subscriptions.current[fieldName]?.();
-      delete subscriptions.current[fieldName];
-    }
-  }, []);
-
-  const valuesProxy = useMemo(() => {
-    function getFieldValue(fieldName: FieldNames<Schema>) {
-      return store.data[fieldName];
-    }
-
-    return new Proxy({} as inferValue<Schema>, {
-      get(_, propertyName) {
-        // In react development mode, all component properties are inspected,
-        // which means the proxy will be triggered even if the property is not used by the form.
-        // This means we must identify an intentional use of the proxy by looking for field names.
-        const fieldName = propertyName as FieldNames<Schema>;
-        if (!allFieldNames.get(fieldName)) {
-          return _[propertyName]; // Returns standard object property values
+  const unsubscribeAll = useCallback(
+    ({ clearTouched }: { clearTouched?: boolean } = {}) => {
+      for (const [fieldName, unsub] of touchedFields.entries()) {
+        unsub?.();
+        if (clearTouched) {
+          touchedFields.delete(fieldName);
+        } else {
+          touchedFields.set(fieldName, undefined);
         }
+      }
+    },
+    [touchedFields],
+  );
 
-        // Subscribe to the field if not already subscribed
-        if (!subscriptions.current[fieldName]) {
-          subscriptions.current[fieldName] = store.subscribeToSlice(
-            () => getFieldValue(fieldName),
-            forceRefresh,
-          );
-        }
+  const valuesProxy = useMemo(
+    () =>
+      new Proxy({} as Record<PropertyKey, unknown>, {
+        get(_, propertyName) {
+          // In react development mode, all component properties are inspected,
+          // which means the proxy will be triggered even if the property is not used by the form.
+          // This means we must identify an intentional use of the proxy by looking for field names.
+          const fieldName = propertyName as FieldNames<Schema>;
+          if (!allFieldNames.get(fieldName)) {
+            return _[propertyName]; // Returns standard object property values
+          }
 
-        return getFieldValue(fieldName);
-      },
-    });
-  }, [allFieldNames, store]);
+          // Subscribe to the field value if not already subscribed
+          const subscription =
+            touchedFields.get(fieldName) ??
+            store.subscribeToSlice(() => store.data[fieldName], forceRefresh);
 
-  // Clear subscriptions when unmounting
-  useEffect(() => clearSubscriptions, [clearSubscriptions]);
+          // Mark field as touched and remember subscription
+          touchedFields.set(fieldName, subscription);
 
-  // Clear subscriptions before each render so that the subscription record represents all current subscriptions
-  clearSubscriptions();
+          return store.data[fieldName];
+        },
+      }),
+    [allFieldNames, store],
+  );
+
+  useEffect(() => {
+    // Resubscribe to any touched fields when mounting.
+    // This is only utilized when react triggers re-rendering during strict mode
+    for (const [fieldName, isSubbed] of touchedFields.entries()) {
+      if (!isSubbed) {
+        touchedFields.set(
+          fieldName,
+          store.subscribeToSlice(() => store.data[fieldName], forceRefresh),
+        );
+      }
+    }
+
+    // Clear subscriptions when unmounting
+    return () => unsubscribeAll();
+  }, [unsubscribeAll, store, touchedFields]);
+
+  // Clear subscriptions before each render so that the latest render controls which fields to subscribe to
+  unsubscribeAll({ clearTouched: true });
 
   return valuesProxy;
 }
