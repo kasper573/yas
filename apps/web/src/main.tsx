7c79d94fce25fcb968c4286563faa524ee8a28d7
ksandin <kasper.sandin@gmail.com>
2023-08-20 22:21:05 +0200

    Add missing StrictMode to apps/web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { resetStyles } from "@yas/ui";
+import { StrictMode } from "react";
 import { App } from "./App";
 
 resetStyles();
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(<App />);
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 } else {
   document.writeln("Could not find root element");
 }
