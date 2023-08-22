aee24ec82a967603eee696a4078aa444cfff9f03
ksandin <kasper.sandin@gmail.com>
2023-08-22 22:26:58 +0200

    Convert to typescript

---
+++ apps/docusaurus/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["yas"],
+  rules: {
+    "unused-imports/no-unused-imports": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
--- apps/docusaurus/src/components/HomepageFeatures/index.tsx
+++ apps/docusaurus/src/components/HomepageFeatures/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Mountain from "@site/static/img/undraw_docusaurus_mountain.svg";
+import Tree from "@site/static/img/undraw_docusaurus_tree.svg";
+import Powered from "@site/static/img/undraw_docusaurus_react.svg";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Svg: Mountain,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -14,8 +17,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Focus on What Matters",
+    Svg: Tree,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -24,8 +27,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Powered by React",
+    Svg: Powered,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -35,9 +38,9 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
