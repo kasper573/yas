a7bea665426409b7fd3b1132264f91e611720823
ksandin <kasper.sandin@gmail.com>
2023-08-23 22:55:18 +0200

    Update home page design

--- apps/docusaurus/src/pages/index.tsx
+++ apps/docusaurus/src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               <Feature
                 title="react-composable-form"
                 image={jigsawUrl}
-                href="/docs/react-composable-form/"
+                href="/docs/react-composable-form/intro"
               >
                 Form library for React with focus on composition, convention and
                 type safety.
@@ -34,7 +34,7 @@ export default function Home() {
               <Feature
                 title="react-imperative-hook"
                 image={stepsUrl}
-                href="/docs/react-imperative-hook/"
+                href="/docs/react-imperative-hook/intro"
               >
                 React hook for improved DX with async UI flows like modals,
                 toasts, drawers, etc.
@@ -49,19 +49,19 @@ export default function Home() {
 
 function Feature({ image, title, children, href }) {
   return (
-    <div className={clsx("col col--6")}>
-      <Link to={href} className="text--center" style={{ display: "block" }}>
+    <div className={clsx("col col--6 text--center", styles.feature)}>
+      <Link to={href} style={{ display: "block" }}>
         <img
           src={image}
           className={styles.featureImage}
           role="img"
           alt={`${title} image`}
         />
-      </Link>
-      <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
-        <p>{children}</p>
-      </div>
+      </Link>
+      <p>{children}</p>
     </div>
   );
 }
+
+const H3 = (props) => <h3 {...props} />;
