7ca0c5e6a43fa2ab7e0b69f24b5be705866fa019
ksandin <kasper.sandin@gmail.com>
2023-09-01 22:51:22 +0200

    Export simplified interface from react-composable-form

--- packages/react-composable-form/src/index.ts
+++ packages/react-composable-form/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./createRCF";
+export { createForm } from "./createForm";
+export type { FieldProps } from "./types/optionTypes";
 export type { inferFormValue } from "./createForm";
 export type { FormValidationMode } from "./types/commonTypes";
 export { formValidationModes } from "./types/commonTypes";
