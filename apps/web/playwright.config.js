0319d1ee769e277e8140ff714f4fa71251f091c0
ksandin <kasper.sandin@gmail.com>
2023-09-02 00:56:23 +0200

    Switch back to pnpm

--- apps/web/playwright.config.js
+++ apps/web/playwright.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from "@yas/playwright/preset";
 export default defineConfig({
   baseURL: "http://localhost:3002",
   webServers: [
-    { command: "yarn dev", url: "http://localhost:3002" },
+    { command: "pnpm dev", url: "http://localhost:3002" },
     {
-      command: "yarn workspace @yas/api dev",
+      command: "pnpm --filter @yas/api dev",
       url: "http://localhost:1234/healthz",
     },
   ],
