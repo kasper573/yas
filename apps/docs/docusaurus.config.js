c241e389df2dbace1548b8edf46d93c42f19d368
ksandin <805006+ksandin@users.noreply.github.com>
2023-09-02 23:52:07 +0200

    Add twoslash (#50)
    * Add twoslash
    
    * Remove build step
    
    Testing without having build step on CI for a while. Vercel will use it externally anyway. I'm curious to see if this stops breaking the docusaurus build on vercel.
    
    * Fix formatting

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 // @ts-expect-error Prisms types seem to not play well with Docusaurus, but they work. Just silence this error.
 const { themes: prismThemes } = require("prism-react-renderer");
 const getGitBranchName = require("current-git-branch");
+const remarkShikiTwoslash = require("remark-shiki-twoslash").default;
 const { projects } = require("./fixtures/projects");
 
 const branchName = getGitBranchName();
@@ -13,78 +14,88 @@ const pathToAppRelativeToRoot = __dirname
   .replace(rootDir, "")
   .replaceAll("\\", "/");
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: "Yet Another Stack",
-  tagline: "A collection of React and Typescript libraries",
-  favicon: "img/favicon.ico",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  organizationName: "ksandin",
-  projectName: "yas",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "throw",
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+module.exports = async function createConfig() {
+  // Using async import since it is allows us to import ESM modules in CommonJS modules.
+  // https://github.com/facebook/docusaurus/issues/5379#issuecomment-1525220908
+  const { nodeTypes } = await import("@mdx-js/mdx");
+  const rehypeRaw = (await import("rehype-raw")).default;
 
-  plugins: ["docusaurus-plugin-sass"],
+  /** @type {import('@docusaurus/types').Config} */
+  const config = {
+    title: "Yet Another Stack",
+    tagline: "A collection of React and Typescript libraries",
+    favicon: "img/favicon.ico",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    organizationName: "ksandin",
+    projectName: "yas",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "throw",
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    plugins: ["docusaurus-plugin-sass"],
+
+    presets: [
+      [
+        "classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./fixtures/sidebars.js"),
+            editUrl: `${yasGithubUrl}/tree/${branchName}${pathToAppRelativeToRoot}`,
+            remarkPlugins: [[remarkShikiTwoslash, { theme: "dark-plus" }]],
+            // Using rehype-raw to make remark-shiki-twoslash work with mdx v2
+            // (Works around this error: https://github.com/shikijs/twoslash/issues/125)
+            rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
+          },
+          theme: {
+            customCss: require.resolve("./src/styles/global.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve("./fixtures/sidebars.js"),
-          editUrl: `${yasGithubUrl}/tree/${branchName}${pathToAppRelativeToRoot}`,
+        image: "img/card.png",
+        navbar: {
+          title: "YAS",
+          logo: {
+            alt: "YAS Logo",
+            src: "img/logo.png",
+          },
+          items: [
+            ...projects
+              .filter((p) => p.sidebar)
+              .map(({ sidebar: [sidebarId], title }) => ({
+                type: "docSidebar",
+                sidebarId,
+                position: "left",
+                label: title,
+              })),
+            {
+              href: yasGithubUrl,
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        theme: {
-          customCss: require.resolve("./src/main.css"),
+        prism: {
+          theme: prismThemes.github,
+          darkTheme: prismThemes.dracula,
+          defaultLanguage: "typescript",
+        },
+        liveCodeBlock: {
+          playgroundPosition: "bottom",
+        },
+        footer: {
+          copyright: `Made by <a href="https://github.com/ksandin">@ksandin</a> using <a href="https://docusaurus.io/">Docusaurus</a>.`,
         },
       }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      image: "img/card.png",
-      navbar: {
-        title: "YAS",
-        logo: {
-          alt: "YAS Logo",
-          src: "img/logo.png",
-        },
-        items: [
-          ...projects
-            .filter((p) => p.sidebar)
-            .map(({ sidebar: [sidebarId], title }) => ({
-              type: "docSidebar",
-              sidebarId,
-              position: "left",
-              label: title,
-            })),
-          {
-            href: yasGithubUrl,
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-        defaultLanguage: "typescript",
-      },
-      liveCodeBlock: {
-        playgroundPosition: "bottom",
-      },
-      footer: {
-        copyright: `Made by <a href="https://github.com/ksandin">@ksandin</a> using <a href="https://docusaurus.io/">Docusaurus</a>.`,
-      },
-    }),
+  };
+  return config;
 };
-
-module.exports = config;
