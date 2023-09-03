fdc85dffd2b0ebaaf129571da3b43d4f00f1d5cb
ksandin <kasper.sandin@gmail.com>
2023-09-03 03:15:32 +0200

    Export FormLayoutProps type

--- packages/react-composable-form/src/index.ts
+++ packages/react-composable-form/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./createRCF";
 export { createForm } from "./createForm";
-export type { FieldProps } from "./types/optionTypes";
+export type { FieldProps, FormLayoutProps } from "./types/optionTypes";
 export type { inferFormValue } from "./createForm";
 export type { FormValidationMode } from "./types/commonTypes";
 export { formValidationModes } from "./types/commonTypes";
