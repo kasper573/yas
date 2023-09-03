7517e909e677b2eff3182b572af51b08f11b856e
ksandin <kasper.sandin@gmail.com>
2023-09-03 03:20:23 +0200

    Overview page for react-composable-form docs

--- apps/docs/docs/react-composable-form/README.components.tsx
+++ apps/docs/docs/react-composable-form/README.components.tsx
@@ -1,23 +1,70 @@
-import { createForm } from "react-composable-form";
 import { z } from "zod";
+import { useState } from "react";
+import type { inferFormValue } from "react-composable-form";
+import { createForm } from "react-composable-form";
+import { TextField } from "../../src/fields/TextField";
+import { NumberField } from "../../src/fields/NumberField";
 
-export const MyForm = createForm((options) =>
+const BaseForm = createForm((options) =>
   options
-    // TextField, NumberField and SwitchField are all react components.
-    // Ignore where they come from for now, we'll explain how to create them later.
+    .validateOn("blur", "submit")
     .type(z.string(), TextField)
     .type(z.number(), NumberField)
-    .type(z.boolean(), SwitchField),
+    .type(z.string().brand("password"), TextField, { type: "password" }),
 );
 
-export function TextField() {
-  return null;
+const UserForm = BaseForm.extend((options) =>
+  options
+    .schema(
+      z.object({
+        email: z.string().email(),
+        password: z.string().brand("password"),
+        age: z.number().optional(),
+      }),
+    )
+    .layout(({ fields: { Email, Password, Age }, reset, handleSubmit }) => (
+      <form onReset={reset} onSubmit={handleSubmit} style={styles.grid}>
+        <Email style={styles.leanLeft} />
+        <Password style={styles.leanRight} />
+        <Age />
+        <div className="button-group padding-top--md" style={styles.dockRight}>
+          <button className="button button--secondary" type="reset">
+            Reset
+          </button>
+          <button className="button button--primary" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    )),
+);
+
+export function App() {
+  type FormState = inferFormValue<typeof UserForm>;
+  const [current, setCurrent] = useState<FormState>();
+  const [submitted, submit] = useState<FormState>();
+  return (
+    <>
+      <h3>Form</h3>
+      <UserForm value={current} onChange={setCurrent} onSubmit={submit} />
+
+      <h3 style={{ marginTop: "1rem" }}>Current state</h3>
+      <pre>{JSON.stringify(current, null, 2)}</pre>
+
+      <h3 style={{ marginTop: "1rem" }}>Last submit</h3>
+      <pre>{JSON.stringify(submitted, null, 2)}</pre>
+    </>
+  );
 }
 
-function NumberField() {
-  return null;
-}
-
-function SwitchField() {
-  return null;
-}
+const styles = {
+  grid: { display: "grid" },
+  dockRight: { marginLeft: "auto" },
+  leanLeft: { transformOrigin: "left", transform: "rotateZ(-10deg)" },
+  leanRight: {
+    transformOrigin: "right",
+    transform: "rotateZ(10deg)",
+    width: "50%",
+    marginLeft: "auto",
+  },
+};
