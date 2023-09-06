a92efe2442b09a939da8510946cb24975ce4ecf6
ksandin <kasper.sandin@gmail.com>
2023-09-06 06:01:08 +0200

    Log embed

--- apps/api/tsup.config.cjs
+++ apps/api/tsup.config.cjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "tsup";
 import { loadEnv } from "@yas/env/utils";
 
+const env = loadEnv(process.cwd(), (key, value) => [key, value ?? ""]);
+
+console.log("Will embed the following env in the API bundle:\n" + JSON.stringify(env, null, 2));
+
 export default defineConfig({
   outExtension: () => ({ js: `.js` }),
   entry: { index: "src/index.ts" },
   format: "esm",
   clean: true,
   noExternal: [/^@yas\//],
-  env: loadEnv(process.cwd(), (key, value) => [key, value ?? ""]),
+  env,
 });
