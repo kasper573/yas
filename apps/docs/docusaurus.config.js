dcf62f7637f7a85f9ce1598851bc26d938314df5
ksandin <805006+ksandin@users.noreply.github.com>
2023-09-08 00:13:57 +0200

    Improved shiki-twoslash integration with Docusaurus 3 (#63)
    * Start working on custom ast based remask-shiki-twoslash plugin
    
    * Custom remark plugin for shiki-twoslash
    
    * Add example usage
    
    * Use file dirname as vfsRoot
    
    * Use remark-shiki-twoslash and create html-to-jsx plugin to solve the issue
    
    * Fix theme bug in shiki-twoslash-loader

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -1,9 +1,4 @@
-// @ts-check
 const path = require("path");
-
-// @ts-expect-error Prisms types seem to not play well with Docusaurus, but they work. Just silence this error.
-const { themes: prismThemes } = require("prism-react-renderer");
-const remarkShikiTwoslash = require("remark-shiki-twoslash").default;
 const { projects } = require("./fixtures/projects");
 const { env } = require("./src/env");
 
@@ -12,99 +7,84 @@ const pathToAppRelativeToRoot = __dirname
   .replace(rootDir, "")
   .replaceAll("\\", "/");
 
-module.exports = async function createConfig() {
-  // Using async import since it is allows us to import ESM modules in CommonJS modules.
-  // https://github.com/facebook/docusaurus/issues/5379#issuecomment-1525220908
-  const { nodeTypes } = await import("@mdx-js/mdx");
-  const rehypeRaw = (await import("rehype-raw")).default;
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Yet Another Stack",
+  tagline: "A collection of React and Typescript libraries",
+  favicon: "img/favicon.ico",
+  url: env.docsUrl,
+  baseUrl: "/",
+  organizationName: env.git.owner,
+  projectName: env.git.project,
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-  /** @type {import('@docusaurus/types').Config} */
-  const config = {
-    title: "Yet Another Stack",
-    tagline: "A collection of React and Typescript libraries",
-    favicon: "img/favicon.ico",
-    url: env.docsUrl,
-    baseUrl: "/",
-    organizationName: env.git.owner,
-    projectName: env.git.project,
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "throw",
-    i18n: {
-      defaultLocale: "en",
-      locales: ["en"],
-    },
+  plugins: [
+    "docusaurus-plugin-sass",
+    env.analytics ? "@gracefullight/docusaurus-plugin-vercel-analytics" : false,
+  ],
 
-    plugins: [
-      "docusaurus-plugin-sass",
-      env.analytics
-        ? "@gracefullight/docusaurus-plugin-vercel-analytics"
-        : false,
-    ],
-
-    presets: [
-      [
-        "classic",
-        /** @type {import('@docusaurus/preset-classic').Options} */
-        ({
-          docs: {
-            sidebarPath: require.resolve("./fixtures/sidebars.js"),
-            editUrl: env.git.sourceUrl(pathToAppRelativeToRoot),
-            remarkPlugins: [
-              [remarkShikiTwoslash, require("./shiki-twoslash.config")],
-            ],
-            // Using rehype-raw to make remark-shiki-twoslash work with mdx v2
-            // (Works around this error: https://github.com/shikijs/twoslash/issues/125)
-            rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
-          },
-          theme: {
-            customCss: require.resolve("./src/styles/global.scss"),
-          },
-        }),
-      ],
-    ],
-
-    themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        image: "img/card.png",
-        navbar: {
-          title: "YAS",
-          logo: {
-            alt: "YAS Logo",
-            src: "img/logo.webp",
-            width: 32,
-            height: 32,
-          },
-          items: [
-            ...projects
-              .filter((p) => p.sidebar)
-              .map(({ sidebar: [sidebarId], title }) => ({
-                type: "docSidebar",
-                sidebarId,
-                position: "left",
-                label: title,
-              })),
-            {
-              href: env.git.projectUrl,
-              label: "GitHub",
-              position: "right",
-            },
+        docs: {
+          sidebarPath: require.resolve("./fixtures/sidebars.js"),
+          editUrl: env.git.sourceUrl(pathToAppRelativeToRoot),
+          remarkPlugins: [
+            [
+              require("remark-shiki-twoslash").default,
+              require("./shiki-twoslash.config"),
+            ],
+            require("remark-html-to-jsx"), // Transforms HTML nodes output by shiki-twoslash into JSX nodes
           ],
         },
-        prism: {
-          theme: prismThemes.github,
-          darkTheme: prismThemes.dracula,
-          defaultLanguage: "typescript",
-        },
-        liveCodeBlock: {
-          playgroundPosition: "bottom",
-        },
-        footer: {
-          copyright:
-            `Made by <a href="${env.git.ownerUrl}">@${env.git.owner}</a> ` +
-            `using <a href="https://docusaurus.io/">Docusaurus</a>.`,
+        theme: {
+          customCss: require.resolve("./src/styles/global.scss"),
         },
       }),
-  };
-  return config;
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: "img/card.png",
+      navbar: {
+        title: "YAS",
+        logo: {
+          alt: "YAS Logo",
+          src: "img/logo.webp",
+          width: 32,
+          height: 32,
+        },
+        items: [
+          ...projects
+            .filter((p) => p.sidebar)
+            .map(({ sidebar: [sidebarId], title }) => ({
+              type: "docSidebar",
+              sidebarId,
+              position: "left",
+              label: title,
+            })),
+          {
+            href: env.git.projectUrl,
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        copyright:
+          `Made by <a href="${env.git.ownerUrl}">@${env.git.owner}</a> ` +
+          `using <a href="https://docusaurus.io/">Docusaurus</a>.`,
+      },
+    }),
 };
+
+module.exports = config;
---
+++ packages/shiki-twoslash-loader/loader.js
@@ -0,0 +1,104 @@
+// @ts-check
+
+const fs = require("fs");
+const path = require("path");
+const shiki = require("shiki");
+const st = require("shiki-twoslash");
+
+const highlighterMap = new Map();
+const userSettings = getUserSettings();
+
+module.exports = function shikiTwoslashLoader(code) {
+  const callback = this.async();
+
+  const vfsRoot = path.dirname(this.resourcePath);
+  const settings = { vfsRoot, themes: [], ...userSettings };
+
+  getHighlighters(settings.themes).then((highlighters) => {
+    const result = renderHtml("tsx", code, settings, highlighters);
+    if (result.type === "error") {
+      callback(new Error(result.message));
+    } else {
+      callback(null, htmlStringToReactElementModule(result.html));
+    }
+  });
+};
+
+function htmlStringToReactElementModule(html) {
+  return `
+    import React from 'react';
+    export default React.createElement("div", { 
+      dangerouslySetInnerHTML: { __html: ${JSON.stringify(html)} }
+    });
+  `;
+}
+
+function getUserSettings() {
+  const configFile = path.join(process.cwd(), "shiki-twoslash.config.js");
+  if (fs.existsSync(configFile)) {
+    return require(configFile) ?? {};
+  }
+  return {};
+}
+
+/**
+ * @param {string} theme
+ * @return {Promise<import("shiki").Highlighter>}
+ */
+async function getHighlighter(theme) {
+  let existing = highlighterMap.get(theme);
+  if (!existing) {
+    existing = shiki.getHighlighter({ theme });
+    highlighterMap.set(theme, existing);
+  }
+  return existing;
+}
+
+/**
+ * @param {string[]} themes
+ * @return {Promise<Record<string, import("shiki").Highlighter>>}
+ */
+function getHighlighters(themes) {
+  return Promise.all(themes.map(getHighlighter))
+    .then((highlighters) => highlighters.map((h, i) => [themes[i], h]))
+    .then(Object.fromEntries);
+}
+
+/**
+ * @param {string} lang
+ * @param {string} code
+ * @param {import("shiki-twoslash").UserConfigSettings} settings
+ * @param {Record<string, import("shiki").Highlighter>} highlightersByTheme
+ * @return {{type: "error", message: string}|{type: "success", html: string}}
+ */
+function renderHtml(lang, code, settings, highlightersByTheme) {
+  let twoslash;
+
+  try {
+    twoslash = st.runTwoSlash(code, lang, settings);
+    if (twoslash) {
+      code = twoslash.code;
+    }
+  } catch (e) {
+    return { type: "error", message: e.message };
+  }
+
+  const entries = Object.entries(highlightersByTheme);
+  if (!entries.length) {
+    return { type: "error", message: "Cannot render twoslash without themes" };
+  }
+
+  let html = "";
+  for (const [themeName, highlighter] of entries) {
+    html += st.renderCodeToHTML(
+      code,
+      lang,
+      { twoslash: true },
+      { ...settings, themeName },
+      highlighter,
+      twoslash,
+    );
+  }
+
+  return { type: "success", html };
+}
