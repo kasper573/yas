2e2c60500c1fa7def10762f409acd484ae8d3810
ksandin <kasper.sandin@gmail.com>
2023-09-11 23:40:20 +0200

    Remove unnecessary typedef

--- apps/docs/types.d.ts
+++ apps/docs/types.d.ts
@@ -7,8 +7,3 @@ declare module "*.webp" {
   const imageUrl: string;
   export default imageUrl;
 }
-
-declare module "*.scss" {
-  const classNames: Record<string, string>;
-  export default classNames;
-}
