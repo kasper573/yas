9ecdabfb8ced529a60b998a917c2fbd673317074
ksandin <kasper.sandin@gmail.com>
2023-09-22 00:24:52 +0200

    Fix formatting

--- packages/vanilla-extract-styled/vitest.config.mjs
+++ packages/vanilla-extract-styled/vitest.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     vanillaExtractPlugin({
       // When a debugId is missing it's not technically always a recipe, but in our test suite it is.
       // This is a bit of a hack to be able to somewhat gracefully test recipe outputs.
-      identifiers: ({ debugId }) => debugId ?? recipeClassName
+      identifiers: ({ debugId }) => debugId ?? recipeClassName,
     }),
   ],
 });
