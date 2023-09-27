5740dc6639e0e3863d02e4806a5f296d2fddc907
ksandin <kasper.sandin@gmail.com>
2023-09-28 00:33:34 +0200

    Correct path to playwright cli

--- packages/yas-playwright/cli.js
+++ packages/yas-playwright/cli.js
@@ -3,4 +3,4 @@
 // This file is a proxy to @playwright/test/lib/cli to work around @playwright/test failing
 // whenever a project also has the playwright package installed.
 // See https://github.com/microsoft/playwright/issues/23314
-require("@playwright/test/lib/cli");
+require("@playwright/test/cli");
