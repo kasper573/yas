783f4f95279082c5389a24100050cb36e53d3387
ksandin <kasper.sandin@gmail.com>
2023-10-05 23:00:30 +0200

    Promote use of css framework

--- libs/yas-css/src/index.ts
+++ libs/yas-css/src/index.ts
@@ -1,20 +1,20 @@
 // This is an encapsulation of vanilla-extract modules.
 // You should never use vanilla-extract directly (enforced via linting).
 
-// Re-export our chosen styling libraries as-is
-export * from "@vanilla-extract/css";
-export * from "clsx";
-
-// Export our custom utilities
-import { recipe } from "@vanilla-extract/recipes";
+import { style } from "@vanilla-extract/css";
+import { recipe as unsafeRecipe } from "@vanilla-extract/recipes";
 import { createStyledFactory } from "vanilla-extract-styled";
 import { createRecipeFactory } from "vanilla-extract-recipe-factory";
 import * as tokens from "./tokens";
 import { atoms } from "./atoms.css";
 
+// Our convention is to prioritize relying on our atomic CSS framework.
 const styled = createStyledFactory(atoms);
-const atomicRecipe = createRecipeFactory(atoms);
+const recipe = createRecipeFactory(atoms);
 
-export { tokens, styled, recipe, atomicRecipe };
+// But we provide non-atomic escape hatches via a clear naming convention.
+const unsafe = { style, recipe: unsafeRecipe };
+
+export { tokens, styled, atoms, recipe, unsafe };
 export { destructureVariantProps, variantProps } from "vanilla-extract-styled";
 export * from "./atoms.css";
