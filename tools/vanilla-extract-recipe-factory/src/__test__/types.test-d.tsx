246fbd76d943ee2e738bd0a628b62b19884ea079
ksandin <kasper.sandin@gmail.com>
2023-10-07 04:21:57 +0200

    Fix type inference issue with default variants

--- tools/vanilla-extract-recipe-factory/src/__test__/types.test-d.tsx
+++ tools/vanilla-extract-recipe-factory/src/__test__/types.test-d.tsx
@@ -1,5 +1,6 @@
 import { expectTypeOf } from "vitest";
 import type { RuntimeFn } from "@vanilla-extract/recipes";
+import { createRecipeFactory } from "../index";
 import { customRecipe, regularRecipe } from "./test.css";
 
 it("identical custom and regular recipes produce the same variant types", () => {
@@ -12,6 +13,47 @@ it("identical custom and regular recipes produce the same variant types (alterna
   type RegularVariants = inferVariants<typeof regularRecipe>;
   type CustomVariants = inferVariants<typeof customRecipe>;
   expectTypeOf<RegularVariants>().toEqualTypeOf<CustomVariants>();
+});
+
+it("supports rich set of variants and defaultVariants", () => {
+  createRecipeFactory((style: object) => "whatever")({
+    variants: {
+      size: {
+        small: {
+          fontSize: "x",
+        },
+        medium: {
+          fontSize: "y",
+        },
+      },
+      color: {
+        primary: {
+          background: "a",
+        },
+        secondary: {
+          background: "b",
+        },
+      },
+      variant: {
+        contained: {
+          borderRadius: 123,
+        },
+        outlined: {
+          background: false,
+        },
+      },
+      disabled: {
+        true: {},
+        false: {},
+      },
+    },
+    defaultVariants: {
+      size: "small",
+      variant: "outlined",
+      color: "primary",
+      disabled: false,
+    },
+  });
 });
 
 type inferVariants<T> = T extends RuntimeFn<infer Groups>
