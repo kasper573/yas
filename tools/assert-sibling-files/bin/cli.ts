66cdb9c04f7f9a784d27b3d4b825048a2fe74a7e
ksandin <kasper.sandin@gmail.com>
2023-10-12 20:35:21 +0200

    Require stories for all ui components

---
+++ tools/assert-sibling-files/bin/cli.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env tsx
+
+import path from "path";
+import { glob } from "glob";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+async function run(
+  filePattern: string,
+  siblingSuffix: string,
+): Promise<number> {
+  const targetFiles = (await glob(filePattern)).filter(
+    (file) => !path.parse(file).name.endsWith(siblingSuffix),
+  );
+
+  console.log(
+    `🚀  Making sure ${targetFiles.length} ${filePattern} files have ${siblingSuffix} sibling files`,
+  );
+
+  const hasSiblings = await Promise.all(targetFiles.map(hasSibling));
+  const missingSiblings = targetFiles.filter((_, i) => !hasSiblings[i]);
+
+  if (missingSiblings.length) {
+    console.error(
+      `❌  Missing ${siblingSuffix} files for:\n${missingSiblings
+        .map((file) => `  ${file}`)
+        .join("\n")}`,
+    );
+    return 1;
+  }
+
+  console.log(`✅  All ${filePattern} files have ${siblingSuffix} siblings`);
+
+  return 0;
+
+  async function hasSibling(targetFIle: string): Promise<boolean> {
+    const target = path.parse(targetFIle);
+    const siblingGlob = path.join(target.dir, target.name + siblingSuffix);
+    return (await glob(siblingGlob)).length > 0;
+  }
+}
+
+const args = yargs(hideBin(process.argv))
+  .usage(
+    "Makes sure all files matching a pattern have a sibling file with the given suffix",
+  )
+  .options({
+    filePattern: {
+      type: "string",
+      alias: "pattern",
+      demandOption: true,
+    },
+    siblingSuffix: {
+      type: "string",
+      alias: "suffix",
+      demandOption: true,
+    },
+  })
+  .parseSync();
+
+run(args.filePattern, args.siblingSuffix).then(process.exit);
