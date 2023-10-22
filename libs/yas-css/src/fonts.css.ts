ff34d3f2a2bb82dbb4d1dd30e0a815866d11770b
ksandin <kasper.sandin@gmail.com>
2023-10-22 04:50:41 +0200

    Update font files

--- libs/yas-css/src/fonts.css.ts
+++ libs/yas-css/src/fonts.css.ts
@@ -1,11 +1,11 @@
 import { fontFace } from "@vanilla-extract/css";
-import inter900NormalWoff2 from "./fonts/inter-latin-ext-900-normal.woff2";
-import caveat700NormalWoff2 from "./fonts/caveat-latin-700-normal.woff2";
+import inter400 from "./fonts/inter-latin-400-normal.woff2";
+import caveat400 from "./fonts/caveat-latin-400-normal.woff2";
 
 export const inter = fontFace({
-  src: [`url(${inter900NormalWoff2}) format("woff2")`],
+  src: [`url(${inter400}) format("woff2")`],
 });
 
 export const caveat = fontFace({
-  src: [`url(${caveat700NormalWoff2}) format("woff2")`],
+  src: [`url(${caveat400}) format("woff2")`],
 });
