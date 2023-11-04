28775143c40dcea4200d3ba107d65bc0b1325237
ksandin <kasper.sandin@gmail.com>
2023-11-04 23:37:18 +0100

    Fix invalid test expectations

--- tools/vanilla-extract-constrained/cypress/tests/integration.cy.tsx
+++ tools/vanilla-extract-constrained/cypress/tests/integration.cy.tsx
@@ -10,36 +10,37 @@ import {
 
 it("can define valid value", () => {
   const container = render(<div className={validRedColor} />);
-  container.should("have.css", { color: colors.red });
+  container.should("have.css", "color", colors.red);
 });
 
 it("can define valid aliased value", () => {
-  render(<div className={validAliasedRedBackground} />).should("have.css", {
-    background: colors.red,
-  });
+  render(<div className={validAliasedRedBackground} />).should(
+    "have.css",
+    "background-color",
+    colors.red,
+  );
 });
 
 it("conditional value uses default when condition is inactive", () => {
-  render(<div className={conditionalRedColor} />).should("have.css", {
-    color: colors.blue,
-  });
+  render(<div className={conditionalRedColor} />).should(
+    "have.css",
+    "color",
+    colors.blue,
+  );
 });
 
 it("conditional value is correct when condition is active", () => {
   render(<div className={conditionalRedColor} data-condition />).should(
     "have.css",
-    { color: colors.red },
+    "color",
+    colors.red,
   );
 });
 
 it("can define multiple properties", () => {
-  render(<div className={validRedColorAndGreenBackground} />).should(
-    "have.css",
-    {
-      color: colors.red,
-      backgroundColor: colors.green,
-    },
-  );
+  render(<div className={validRedColorAndGreenBackground} />)
+    .should("have.css", "color", colors.red)
+    .should("have.css", "background-color", colors.green);
 });
 
 it("defining multiple properties yield a single class name", () => {
@@ -53,13 +54,13 @@ function render(element: JSX.Element) {
     cloneElement(element, {
       "data-testid": "root",
       style: testElementDefaultStyle,
+      children: "Element",
     }),
   );
   return cy.get(`[data-testid="root"]`);
 }
 
 const testElementDefaultStyle = {
-  width: 50,
-  height: 50,
+  padding: 10,
   border: "1px solid black",
 };
