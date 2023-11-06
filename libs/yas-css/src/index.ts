012e1a847ae6e720d5ffe79302a2b2839369b3af
ksandin <kasper.sandin@gmail.com>
2023-11-06 08:47:03 +0100

    Use constrained in design system

--- libs/yas-css/src/index.ts
+++ libs/yas-css/src/index.ts
@@ -5,6 +5,7 @@ import { style, globalStyle, keyframes } from "@vanilla-extract/css";
 import { recipe as unsafeRecipe } from "@vanilla-extract/recipes";
 import { createStyledFactory } from "vanilla-extract-styled";
 import { createRecipeFactory } from "vanilla-extract-recipe-factory";
+import { constrainedStyle } from "./constrained.css";
 import * as tokens from "./tokens";
 import { atoms } from "./atoms.css";
 
@@ -14,8 +15,8 @@ export { clsx } from "clsx";
 
 // Our own design system specific atomic CSS Framework utilities
 export { constrainedStyle } from "./constrained.css";
-export const styled = createStyledFactory(atoms);
-export const recipe = createRecipeFactory(atoms);
+export const styled = createStyledFactory(constrainedStyle);
+export const recipe = createRecipeFactory(constrainedStyle);
 export { tokens, atoms };
 export * from "./themeVars.css";
 export type { Atoms } from "./atoms.css";
