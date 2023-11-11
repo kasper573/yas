a0263c4dfe09a3282bdc40aad769b3aa74c8ab6b
ksandin <kasper.sandin@gmail.com>
2023-11-11 02:08:58 +0100

    Dont use sx prop in BasicFormLayout

--- libs/yas-ui/src/form/BasicFormLayout.tsx
+++ libs/yas-ui/src/form/BasicFormLayout.tsx
@@ -16,28 +16,28 @@ export function BasicFormLayout({
 }: FormLayoutProps & { title: ReactNode; isLoading?: boolean }) {
   return (
     <form onSubmit={handleSubmit}>
-      <Stack direction="row" sx={{ justifyContent: "space-between", mb: "#3" }}>
-        <Text variant="h2">{title}</Text>
-        <Stack direction="row" gap={2} align="center">
-          {isLoading && <CircularProgress />}
-          <Button variant="outlined" onClick={reset}>
-            Reset
-          </Button>
-          <Button disabled={isLoading} type="submit" variant="contained">
-            Submit
-          </Button>
+      <Stack gap={2}>
+        <Stack direction="row" align="center" gap={2}>
+          <Text variant="h2">{title}</Text>
+          <Stack direction="row" gap={2} align="center">
+            {isLoading && <CircularProgress />}
+            <Button variant="outlined" onClick={reset}>
+              Reset
+            </Button>
+            <Button disabled={isLoading} type="submit" variant="contained">
+              Submit
+            </Button>
+          </Stack>
         </Stack>
+        <Stack direction="column" gap={2}>
+          {Object.values(fields).map((Component, index) => (
+            <Component key={index} />
+          ))}
+        </Stack>
+        {generalErrors.length > 0 && (
+          <Alert color="error">{generalErrors.join(", ")}</Alert>
+        )}
       </Stack>
-      <Stack direction="column" gap={2}>
-        {Object.values(fields).map((Component, index) => (
-          <Component key={index} />
-        ))}
-      </Stack>
-      {generalErrors.length > 0 && (
-        <Alert color="error" sx={{ mt: "#2" }}>
-          {generalErrors.join(", ")}
-        </Alert>
-      )}
     </form>
   );
 }
