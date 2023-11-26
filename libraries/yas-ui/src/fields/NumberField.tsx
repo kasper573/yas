fbe1118300d62961aa4b78a60f022889fb2701cd
ksandin <kasper.sandin@gmail.com>
2023-11-26 02:44:48 +0100

    Don't prevent exponent

--- libraries/yas-ui/src/fields/NumberField.tsx
+++ libraries/yas-ui/src/fields/NumberField.tsx
@@ -1,4 +1,3 @@
-import type { KeyboardEvent } from "react";
 import type { FieldProps } from "../form/rcf";
 import type { TextFieldProps } from "./TextField";
 import { TextField } from "./TextField";
@@ -7,31 +6,16 @@ export interface NumberFieldProps
   extends FieldProps<number>,
     Omit<TextFieldProps, keyof FieldProps<unknown> | "type"> {}
 
-export function NumberField({
-  value,
-  onChange,
-  onKeyDown,
-  ...rest
-}: NumberFieldProps) {
+export function NumberField({ value, onChange, ...rest }: NumberFieldProps) {
   return (
     <TextField
       {...rest}
       type="number"
       value={value?.toString()}
-      onKeyDown={(e) => {
-        preventExponent(e);
-        onKeyDown?.(e);
-      }}
       onChange={(text) => {
         const num = parseFloat(text ?? "");
         onChange?.(isNaN(num) ? undefined : num);
       }}
     />
   );
-}
-
-function preventExponent(e: KeyboardEvent<HTMLInputElement>) {
-  if (e.key === "e") {
-    e.preventDefault();
-  }
 }
