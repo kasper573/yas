25d3ce9e88971af7b944b74509af1a6ac18409a0
ksandin <kasper.sandin@gmail.com>
2023-12-10 01:21:22 +0100

    Inline checkbox label

--- libraries/yas-ui/src/fields/CheckboxField.tsx
+++ libraries/yas-ui/src/fields/CheckboxField.tsx
@@ -2,23 +2,36 @@ import type { ComponentProps } from "react";
 import { styled } from "@yas/style";
 import type { FieldProps } from "../form/rcf";
 import { BaseField } from "../form/BaseField";
+import { Stack } from "../layout/Stack";
 import { checkboxRecipe } from "./CheckboxField.css";
+
+export type CheckboxLabelSide = "left" | "right";
 
 export interface CheckboxFieldProps
   extends FieldProps<boolean>,
     Omit<
       ComponentProps<typeof Checkbox>,
-      keyof FieldProps<boolean> | "checked"
-    > {}
+      keyof FieldProps<boolean> | "checked" | "as" | "asProps"
+    > {
+  labelSide?: CheckboxLabelSide;
+}
 
 export function CheckboxField({
   value,
   onChange,
   disabled,
+  labelSide = "left",
   ...rest
 }: CheckboxFieldProps) {
   return (
     <BaseField
+      as={Stack}
+      asProps={{
+        direction: "row",
+        reverse: labelSide === "left",
+        align: "center",
+        gap: 1,
+      }}
       {...rest}
       control={(id) => (
         <Checkbox
