d232a18d3adaa44aaeb9dcbff9b40e0b39bec51c
ksandin <kasper.sandin@gmail.com>
2023-12-23 05:38:20 +0100

    Run web e2e test vs prod build

--- apps/web/playwright.config.js
+++ apps/web/playwright.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "@yas/test/playwright/config";
 import { env } from "./src/env";
 
-const baseURL = "http://localhost:3002";
+const baseURL = "http://localhost:4173";
 export default defineConfig({
   baseURL,
   isCI: env.CI,
   webServers: [
-    { command: "pnpm dev", url: baseURL },
+    { command: "pnpm build && pnpm preview", url: baseURL },
     {
       command: "pnpm --filter @yas/api dev",
       url: getApiHealthUrl(),
