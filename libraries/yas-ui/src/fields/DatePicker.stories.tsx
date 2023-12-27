223a521575b6e9653f4a3e4d21bb84ff83fd5da4
ksandin <kasper.sandin@gmail.com>
2023-12-27 05:28:57 +0100

    Add DatePicker stories

---
+++ libraries/yas-ui/src/fields/DatePicker.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta } from "@storybook/react";
+import { useState } from "react";
+import type { StrictStoryObj } from "../../.storybook/StrictStoryObj";
+import { DatePicker } from "./DatePicker";
+
+export default {
+  title: "fields/DatePicker",
+  component: DatePicker,
+  tags: ["autodocs"],
+} satisfies Meta<typeof DatePicker>;
+
+export const Default: StrictStoryObj<typeof DatePicker> = {
+  render(props) {
+    const [value, setValue] = useState<Date>(() => new Date());
+    return <DatePicker {...props} value={value} onChange={setValue} />;
+  },
+};
