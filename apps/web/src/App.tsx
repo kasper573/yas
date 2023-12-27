4316defc4638f979d566fbcdc181c1fb2efca48d
ksandin <kasper.sandin@gmail.com>
2023-12-27 06:55:10 +0100

    Error boundary

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,22 +1,26 @@
 import { useMemo, useState } from "react";
 import { createApiClient, ApiClientProvider } from "@yas/api-client";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 import { env } from "./env";
 import { AppRoutes } from "./Routes";
 import { ThemeProvider } from "./theme/ThemeProvider";
 import type { ThemeName } from "./theme";
+import { ErrorFallback } from "./ErrorFallback";
 
 export default function App() {
   const [theme, setTheme] = useState<ThemeName>("dark");
   const apiClient = useMemo(() => createApiClient(env.apiUrl), []);
 
   return (
-    <BrowserRouter>
-      <ApiClientProvider value={apiClient}>
-        <ThemeProvider theme={theme} setTheme={setTheme}>
-          <AppRoutes />
-        </ThemeProvider>
-      </ApiClientProvider>
-    </BrowserRouter>
+    <ErrorBoundary fallbackRender={ErrorFallback}>
+      <BrowserRouter>
+        <ApiClientProvider value={apiClient}>
+          <ThemeProvider theme={theme} setTheme={setTheme}>
+            <AppRoutes />
+          </ThemeProvider>
+        </ApiClientProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
---
+++ apps/web/src/ErrorFallback.tsx
@@ -0,0 +1,54 @@
+import type { FallbackProps } from "react-error-boundary";
+
+// Should have no runtime dependencies to be as resilient as possible
+
+export function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <div style={styles.container}>
+      <h2 style={styles.title}>
+        <span role="img" aria-label="sad face">
+          😢
+        </span>{" "}
+        Oops, something went wrong!
+      </h2>
+      <p>We are sorry for the inconvenience. Please try again later.</p>
+      <div style={styles.buttons}>
+        <button onClick={resetErrorBoundary}>Try again</button>
+        <a href="/">Back to home page</a>
+      </div>
+      <details style={styles.details}>
+        <summary style={styles.detailsSummary}>Error Details</summary>
+        <pre style={styles.detailsContent}>{error.stack ?? error.message}</pre>
+      </details>
+    </div>
+  );
+}
+
+const border = "1px solid #ccc";
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  },
+  title: { margin: 0 },
+  buttons: { display: "flex", flexDirection: "row", gap: 10 },
+  details: {
+    marginTop: 20,
+    width: 500,
+    overflow: "auto",
+    border,
+    borderRadius: 8,
+  },
+  detailsSummary: {
+    padding: 12,
+    cursor: "pointer",
+  },
+  detailsContent: {
+    borderTop: border,
+    padding: 12,
+    margin: 0,
+  },
+} as const;
