d08c063ac264ef8a03680fa7b361ac2ca3c3763f
ksandin <kasper.sandin@gmail.com>
2023-12-28 03:41:05 +0100

    Explain container

--- libraries/yas-ui/src/layout/Container.tsx
+++ libraries/yas-ui/src/layout/Container.tsx
@@ -1,4 +1,9 @@
 import { styled } from "@yas/style";
 import { container } from "./Container.css";
 
+/**
+ * Responsive container.
+ *
+ * Limits its width to the most suitable breakpoint for the current window size.
+ */
 export const Container = styled("div", container);
