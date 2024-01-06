9ec212794da831f134e20f924b576867123449f2
ksandin <kasper.sandin@gmail.com>
2024-01-06 07:25:13 +0100

    Create constrained time formatter

--- libraries/yas-time/src/index.ts
+++ libraries/yas-time/src/index.ts
@@ -1,1 +1,15 @@
+import { createConstrainedFormatter } from "./constrainedFormatter";
+
 export * from "date-fns";
+
+const formatPresets = {
+  "long-date": "PPP",
+  "short-date": "PP",
+};
+
+export type FormatPreset = keyof typeof formatPresets;
+
+// Override the default format function with a stricter one.
+const format = createConstrainedFormatter(formatPresets);
+
+export { format };
