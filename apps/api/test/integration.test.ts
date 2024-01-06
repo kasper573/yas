5577bee7fec74ad737e1af5e82fc79507b53ad4a
ksandin <kasper.sandin@gmail.com>
2024-01-06 19:09:45 +0100

    Add missing context to example integration test

--- apps/api/test/integration.test.ts
+++ apps/api/test/integration.test.ts
@@ -1,9 +1,12 @@
 import { describe, it, expect } from "@yas/test/vitest/node";
 import { createExampleRouter } from "../src/modules/example/router";
+import { FakeUserRepository } from "../src/repositories/FakeUserRepository";
 
 describe("integration", () => {
   it("example module can respond to hello", async () => {
-    const example = createExampleRouter().createCaller({});
+    const example = createExampleRouter().createCaller({
+      userRepository: new FakeUserRepository(),
+    });
     const response = await example.hello("hello");
     expect(response?.message).toBe("hello world");
   });
