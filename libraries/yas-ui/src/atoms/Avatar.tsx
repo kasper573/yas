87b922a515df118d379f28f91ba8084ad231d106
ksandin <kasper.sandin@gmail.com>
2024-01-06 08:32:46 +0100

    Fix type error in avatar props type

--- libraries/yas-ui/src/atoms/Avatar.tsx
+++ libraries/yas-ui/src/atoms/Avatar.tsx
@@ -1,12 +1,15 @@
 import { styled } from "@yas/style";
-import type { HTMLAttributes, ImgHTMLAttributes } from "react";
+import type { ImgHTMLAttributes } from "react";
 import { avatar } from "./Avatar.css";
 
 export const Avatar = styled(AvatarImpl, avatar);
 
-type AvatarImplProps =
-  | Omit<ImgHTMLAttributes<HTMLImageElement>, "children">
-  | Omit<HTMLAttributes<HTMLDivElement>, "src">;
+type AvatarImplProps = Omit<
+  ImgHTMLAttributes<HTMLImageElement>,
+  "children" | "src"
+> & {
+  src?: string;
+};
 
 function AvatarImpl(props: AvatarImplProps) {
   const Component = "src" in props ? "img" : "div";
