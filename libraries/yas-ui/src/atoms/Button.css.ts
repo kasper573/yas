c545ef05c05a8ebc9e186060eb3a8ed39593a89a
ksandin <kasper.sandin@gmail.com>
2024-01-07 17:26:28 +0100

    Simplify button css code

--- libraries/yas-ui/src/atoms/Button.css.ts
+++ libraries/yas-ui/src/atoms/Button.css.ts
@@ -1,14 +1,30 @@
-import { recipe, unsafe } from "@yas/style";
+import { createVar, globalStyle, recipe, unsafe } from "@yas/style";
 
-const nonSurfaceColorSetNames = without(
-  unsafe.colorSetNames,
-  "surface",
+const { colorSetNames, vars: themeVars } = unsafe;
+const nonSurfaceColorSetNames = colorSetNames.filter(
+  (value) => value !== "surface",
 ) as Exclude<unsafe.ColorSetName, "surface">[];
 
 export const iconSizes = {
   small: 24,
   medium: 28,
   large: 32,
+};
+
+const buttonVars = {
+  iconSize: createVar("iconSize"),
+  color: {
+    base: {
+      light: createVar("base.light"),
+      main: createVar("base.main"),
+      dark: createVar("base.dark"),
+    },
+    contrast: {
+      light: createVar("contrast.light"),
+      main: createVar("contrast.main"),
+      dark: createVar("contrast.dark"),
+    },
+  },
 };
 
 export const buttonRecipe = recipe({
@@ -24,6 +40,26 @@ export const buttonRecipe = recipe({
     cursor: "pointer",
   },
   variants: {
+    size: {
+      small: {
+        vars: { [buttonVars.iconSize]: `${iconSizes.small}px` },
+        typography: "caption",
+        px: "#2",
+        py: "#1",
+      },
+      medium: {
+        vars: { [buttonVars.iconSize]: `${iconSizes.medium}px` },
+        typography: "body",
+        px: "#3",
+        py: "#1",
+      },
+      large: {
+        vars: { [buttonVars.iconSize]: `${iconSizes.large}px` },
+        typography: "body2",
+        px: "#3",
+        py: "#1",
+      },
+    },
     icon: {
       true: {
         display: "inline-flex",
@@ -34,24 +70,60 @@ export const buttonRecipe = recipe({
         boxSizing: "border-box",
         fontSize: "100%",
         overflow: "hidden",
+        minWidth: buttonVars.iconSize,
+        minHeight: buttonVars.iconSize,
+        maxWidth: buttonVars.iconSize,
+        maxHeight: buttonVars.iconSize,
       },
       false: {},
     },
-    size: {
-      small: {},
-      medium: {},
-      large: {},
-    },
     color: {
-      "surface-contrast": {},
+      "surface-contrast": {
+        vars: assignColorVars(
+          themeVars.color.surface.contrast,
+          themeVars.color.surface.base,
+        ),
+      },
       ...(Object.fromEntries(
-        nonSurfaceColorSetNames.map((name) => [name, {}]),
-      ) as Record<Exclude<unsafe.ColorSetName, "surface">, {}>),
+        nonSurfaceColorSetNames.map((name) => [
+          name,
+          {
+            vars: assignColorVars(
+              themeVars.color[name].base,
+              themeVars.color[name].contrast,
+            ),
+          },
+        ]),
+      ) as Record<Exclude<unsafe.ColorSetName, "surface">, { vars: {} }>),
     },
     variant: {
-      text: {},
-      contained: {},
-      outlined: {},
+      text: {
+        color: buttonVars.color.base.main,
+        background: {
+          default: `transparent`,
+          hover: buttonVars.color.contrast.light,
+          active: buttonVars.color.contrast.main,
+        },
+        borderColor: "transparent",
+      },
+      contained: {
+        background: {
+          default: buttonVars.color.base.light,
+          hover: buttonVars.color.base.main,
+          active: buttonVars.color.base.dark,
+        },
+        color: buttonVars.color.contrast.light,
+        borderColor: buttonVars.color.contrast.light,
+      },
+      outlined: {
+        background: {
+          default: buttonVars.color.contrast.light,
+          hover: buttonVars.color.contrast.main,
+          active: buttonVars.color.contrast.dark,
+        },
+        color: buttonVars.color.base.main,
+        borderColor: buttonVars.color.base.main,
+      },
     },
     shape: {
       circular: {
@@ -62,62 +134,15 @@ export const buttonRecipe = recipe({
       },
     },
     disabled: {
-      true: { pointerEvents: "none" },
-      false: {},
-    },
-  },
-  compoundVariants: [
-    ...typedEntries(iconSizes).map(([size, value]) => ({
-      variants: { icon: true, size },
-      style: {
-        minWidth: value,
-        minHeight: value,
-        maxWidth: value,
-        maxHeight: value,
-      },
-    })),
-    {
-      variants: { icon: false, size: "small" },
-      style: {
-        typography: "caption",
-        px: "#2",
-        py: "#1",
-      },
-    },
-    {
-      variants: { icon: false, size: "medium" },
-      style: {
-        typography: "body",
-        px: "#3",
-        py: "#1",
-      },
-    },
-    {
-      variants: { icon: false, size: "large" },
-      style: {
-        typography: "body2",
-        px: "#3",
-        py: "#1",
-      },
-    },
-    ...variantsForColorSet(
-      "surface-contrast",
-      "surface.contrast",
-      "surface.base",
-    ),
-
-    ...nonSurfaceColorSetNames.flatMap((color) =>
-      variantsForColorSet(color, `${color}.base`, `${color}.contrast`),
-    ),
-    {
-      variants: { disabled: true },
-      style: {
-        background: { default: "info.base.dark" },
+      true: {
+        pointerEvents: "none",
+        background: "info.base.dark",
         color: "highlight",
         borderColor: "surface.base.dark",
       },
+      false: {},
     },
-  ],
+  },
   defaultVariants: {
     size: "medium",
     variant: "contained",
@@ -128,61 +153,18 @@ export const buttonRecipe = recipe({
   },
 });
 
-function variantsForColorSet<
-  ColorVariant extends string,
-  BasePrefix extends string,
-  ContrastPrefix extends string,
->(
-  colorVariant: ColorVariant,
-  basePrefix: BasePrefix,
-  contrastPrefix: ContrastPrefix,
-) {
-  return [
-    {
-      variants: { color: colorVariant, variant: "text" },
-      style: {
-        color: `${basePrefix}.main`,
-        background: {
-          default: `transparent`,
-          hover: `${contrastPrefix}.light`,
-          active: `${contrastPrefix}.main`,
-        },
-        borderColor: "transparent",
-      },
-    },
-    {
-      variants: { color: colorVariant, variant: "contained" },
-      style: {
-        background: {
-          default: `${basePrefix}.light`,
-          hover: `${basePrefix}.main`,
-          active: `${basePrefix}.dark`,
-        },
-        color: `${contrastPrefix}.main`,
-        borderColor: `${contrastPrefix}.main`,
-      },
-    },
-    {
-      variants: { color: colorVariant, variant: `outlined` },
-      style: {
-        background: {
-          default: `${contrastPrefix}.light`,
-          hover: `${contrastPrefix}.main`,
-          active: `${contrastPrefix}.dark`,
-        },
-        color: `${basePrefix}.main`,
-        borderColor: `${basePrefix}.main`,
-      },
-    },
-  ] as const;
+function assignColorVars(base: unsafe.ColorSet, contrast: unsafe.ColorSet) {
+  return {
+    [buttonVars.color.base.light]: base.light,
+    [buttonVars.color.base.main]: base.main,
+    [buttonVars.color.base.dark]: base.dark,
+    [buttonVars.color.contrast.light]: contrast.light,
+    [buttonVars.color.contrast.main]: contrast.main,
+    [buttonVars.color.contrast.dark]: contrast.dark,
+  };
 }
 
-function without<T>(array: T[], ...values: T[]): T[] {
-  return array.filter((value) => !values.includes(value));
-}
-
-function typedEntries<T extends Record<string, unknown>>(
-  obj: T,
-): Array<[keyof T, T[keyof T]]> {
-  return Object.entries(obj) as Array<[keyof T, T[keyof T]]>;
-}
+globalStyle(`${buttonRecipe.classNames.variants.icon.true} > *`, {
+  width: "100%",
+  height: "100%",
+});
