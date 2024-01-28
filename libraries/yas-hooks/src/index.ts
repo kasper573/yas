1b7ea83557893e1d5b0eef4d73df09b380ceea97
ksandin <kasper.sandin@gmail.com>
2024-01-28 04:25:36 +0100

    add useLatest hook

--- libraries/yas-hooks/src/index.ts
+++ libraries/yas-hooks/src/index.ts
@@ -3,3 +3,4 @@ export { useEventListener, useEventCallback } from "usehooks-ts";
 export * from "./useElementBounds";
 export * from "./useDebounce";
 export * from "./useMediaQuery";
+export * from "./useLatest";
