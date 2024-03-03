d526150c65c1cf8b48db8e5e289d006104644d51
ksandin <kasper.sandin@gmail.com>
2024-03-03 07:07:48 +0100

    fix: remove immer as dependency

--- incubations/react-async-modal-hook/src/Store.ts
+++ incubations/react-async-modal-hook/src/Store.ts
@@ -1,9 +1,6 @@
-import type { Draft } from "immer";
-import { produce } from "immer";
-
 export class Store<State> {
   private _listeners = new Set<StoreListener<State>>();
-  private _currentMutation?: { draft: Draft<State> };
+  private _currentMutation?: { draft: State };
 
   get state(): State {
     return this._state;
@@ -13,22 +10,26 @@ export class Store<State> {
 
   mutate(mutator: (state: State) => void): void {
     if (this._currentMutation) {
-      mutator(this._currentMutation.draft as State);
+      mutator(this._currentMutation.draft);
       return;
     }
 
-    const newState = produce(this._state, (draft) => {
-      this._currentMutation = { draft };
-      try {
-        mutator(draft as State);
-      } finally {
-        this._currentMutation = undefined;
-      }
-    });
+    // We actually don't need to clone the state here, but we do it to
+    // ensure that each mutation makes the state become a new object reference.
+    // This makes integrating with React.useSyncExternalStore way possible.
+    const draft = { ...this._state };
 
-    this._state = newState;
+    this._currentMutation = { draft };
+
+    try {
+      mutator(draft);
+    } finally {
+      this._currentMutation = undefined;
+    }
+
+    this._state = draft;
     for (const listener of this._listeners) {
-      listener(newState);
+      listener(this._state);
     }
   }
 
