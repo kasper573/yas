5a662ee6b72bbdac4d85ec6e51f87f1b44f30c13
ksandin <kasper.sandin@gmail.com>
2024-03-09 02:45:21 +0100

    fix: move graphql testing code to its own route

--- apps/web/src/examples/api-tester/GraphQL.tsx
+++ apps/web/src/examples/api-tester/GraphQL.tsx
@@ -1,3 +1,53 @@
+import {
+  graphql,
+  useGraphQLMutation,
+  useGraphQLQuery,
+} from "@yas/graphql-client";
+import { useDebouncedValue } from "@yas/hooks";
+import { Button, NumberField, Text, TextField } from "@yas/ui";
+import { useState } from "react";
+
+const query = graphql(`
+  query GraphQLTester($input: String!) {
+    greet(name: $input)
+    count
+  }
+`);
+
+const increaseQuery = graphql(`
+  mutation IncreaseCount($amount: Int!) {
+    increaseCount(amount: $amount)
+  }
+`);
+
 export default function GraphQL() {
-  return <>GraphQL</>;
+  const [input, setInput] = useState("");
+  const [increaseAmount, setIncreaseAmount] = useState(1);
+  const debouncedInput = useDebouncedValue(input, 300);
+  const { data } = useGraphQLQuery({
+    query,
+    variables: { input: debouncedInput },
+  });
+  const increase = useGraphQLMutation(increaseQuery);
+  return (
+    <>
+      <Text>Customers</Text>
+      <TextField
+        label="send a greeting"
+        value={input}
+        onChange={setInput}
+        required
+      />
+      <NumberField
+        label="increase amount"
+        value={increaseAmount}
+        onChange={setIncreaseAmount}
+        required
+      />
+      <Button onClick={() => increase.mutate({ amount: increaseAmount })}>
+        Increase count
+      </Button>
+      <pre>{JSON.stringify({ responseData: data }, null, 2)}</pre>
+    </>
+  );
 }
