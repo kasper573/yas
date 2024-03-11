a2b1ee0c16209a9d6475ac0351e2184f100bf3ec
ksandin <kasper.sandin@gmail.com>
2024-03-11 03:19:24 +0100

    ci: try once again

--- apps/graphql-server/tsup.config.mjs
+++ apps/graphql-server/tsup.config.mjs
@@ -1,20 +1,38 @@
+import * as fs from "fs/promises";
+import * as path from "path";
 import { createYasTsupConfig } from "@yas/build/tsup.mjs";
+import tsConfig from "./tsconfig.json";
 
 export default createYasTsupConfig(process.cwd(), {
   outExtension: () => ({ js: `.js` }),
   format: "esm",
   entry: { index: "src/entrypoint.ts" },
   dts: false,
-  plugins: [fixPathSeparatorPlugin()],
+  esbuildPlugins: [fixImportsInGeneratedSchema()],
 });
 
-function fixPathSeparatorPlugin() {
+function fixImportsInGeneratedSchema() {
+  const schemaFile = path.resolve(__dirname, tsConfig.grats.tsSchema);
   return {
-    name: "fix-path-separator",
+    name: "fix-imports-in-generated-schema",
     setup(build) {
-      build.onResolve({ filter: /.*/ }, (args) => {
-        const path = args.path.replace(/\\+/g, "/"); // Replace backslashes with forward slashes
-        return { path };
+      build.onLoad({ filter: /.*/ }, async (args) => {
+        const thisFile = path.resolve(args.resolveDir, args.path);
+        if (thisFile !== schemaFile) {
+          return;
+        }
+
+        // Replace \\ with / in imports
+        let contents = await fs.readFile(args.path, "utf8");
+        contents = contents.replaceAll(
+          /(from\s+['"])([^'"]+)(['"])/g,
+          (_, p1, p2, p3) => `${p1}${p2.replaceAll(/\\+/g, "/")}${p3}`,
+        );
+
+        return {
+          contents,
+          loader: "ts",
+        };
       });
     },
   };
