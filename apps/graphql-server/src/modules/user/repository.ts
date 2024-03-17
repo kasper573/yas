f725560670e427aedcd8f76d466c0305a684faae
ksandin <kasper.sandin@gmail.com>
2024-03-17 07:19:57 +0100

    feat: unique names and avatars

--- apps/graphql-server/src/modules/user/repository.ts
+++ apps/graphql-server/src/modules/user/repository.ts
@@ -1,4 +1,5 @@
 import DataLoader from "dataloader";
+import { uniqueNamesGenerator } from "unique-names-generator";
 import type { ID } from "../../types";
 import type { User } from "./model";
 
@@ -7,11 +8,19 @@ export type UserRepository = DataLoader<ID, User>;
 export function createUserRepository(): UserRepository {
   return new DataLoader<ID, User>(async (ids) => {
     return ids.map((userId) => {
+      const name = uniqueNamesGenerator({ dictionaries, seed: userId });
       return {
         userId,
-        name: "John Doe",
-        avatarUrl: `https://picsum.photos/40/40?grayscale&random=${userId}`,
+        name,
+        avatarUrl: `https://api.dicebear.com/8.x/bottts/svg?seed=${encodeURIComponent(name)}`,
       };
     });
   });
 }
+
+const dictionaries: string[][] = [
+  ["big", "small", "tiny", "huge", "gigantic", "enormous", "little", "large"],
+  ["happy", "sad", "angry", "excited", "calm", "relaxed", "sleepy"],
+  ["red", "green", "blue", "yellow", "black", "white", "orange", "purple"],
+  ["dog", "cat", "mouse", "rat", "cow", "elephant", "tiger", "lion", "cheetah"],
+];
