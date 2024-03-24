1825c24806bcf4004b242c5f2996748f5bccafb0
ksandin <kasper.sandin@gmail.com>
2024-03-24 15:08:22 +0100

    fix: correct entry definitions

--- incubations/hookform-controller-proxy/tsup.config.mjs
+++ incubations/hookform-controller-proxy/tsup.config.mjs
@@ -1,3 +1,3 @@
 import { createYasTsupConfig } from "@yas/build/tsup.mjs";
 
-export default createYasTsupConfig({ entry: "src/index.tsx" });
+export default createYasTsupConfig({ entry: { index: "src/index.tsx" } });
