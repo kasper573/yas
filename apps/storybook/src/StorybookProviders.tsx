17a6bd534084e6fedb690518d459e3b3c498b181
ksandin <kasper.sandin@gmail.com>
2024-06-09 06:47:49 +0200

    chore: load env vars properly in storybook

--- apps/storybook/src/StorybookProviders.tsx
+++ apps/storybook/src/StorybookProviders.tsx
@@ -7,7 +7,6 @@ import {
 import { TrpcClientProvider, createTrpcClient } from "@yas/trpc-client";
 import { GraphQLClientContext, createGraphQLClient } from "@yas/graphql-client";
 import { lazy, useEffect, useMemo } from "react";
-import { env } from "./env";
 
 export function StorybookProviders({
   children,
@@ -16,6 +15,7 @@ export function StorybookProviders({
 }) {
   const modalStore = useMemo(() => new ModalStore(), []);
   const clients = useMemo(createClients, []);
+
   return (
     <ModalContext.Provider value={modalStore}>
       <QueryClientProvider client={clients.query}>
@@ -49,20 +49,21 @@ function UnhandledMutationErrorDialogsBehavior() {
   return null;
 }
 
+const urls = {
+  trpc: import.meta.env.STORYBOOK_TRPC_URL,
+  graphql: import.meta.env.STORYBOOK_GRAPHQL_URL,
+};
+
 function createClients() {
   const headers = () => ({ "client-id": "storybook" });
-  const query = createQueryClient(env.mode);
-  const trpc = createTrpcClient({ url: env.trpcServerUrl, headers });
-  const graphql = createGraphQLClient({ url: env.graphqlServerUrl, headers });
+  const query = createQueryClient("development");
+  const trpc = createTrpcClient({ url: urls.trpc, headers });
+  const graphql = createGraphQLClient({ url: urls.graphql, headers });
   return { query, trpc, graphql };
 }
 
-// Avoid importing the devtools in production
-const QueryDevtools =
-  env.mode === "development"
-    ? lazy(() =>
-        import("@yas/query-client/devtools").then((mod) => ({
-          default: mod.QueryDevtools,
-        })),
-      )
-    : () => null;
+const QueryDevtools = lazy(() =>
+  import("@yas/query-client/devtools").then((mod) => ({
+    default: mod.QueryDevtools,
+  })),
+);
