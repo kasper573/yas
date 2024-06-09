0d257987d8ba2df10b73933d5957d6288ec48ecc
ksandin <kasper.sandin@gmail.com>
2024-06-09 09:56:02 +0200

    chore: remove react-styled-factory incubation

--- incubations/react-styled-factory/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./createStyledFactory";
-export type * from "./types";
--- incubations/react-styled-factory/src/merge.ts
+++
@@ -1,6 +0,0 @@
-export function merge<T extends object>(a?: T, b?: T): T | undefined {
-  if (a === b) {
-    return a;
-  }
-  return a && b ? { ...a, ...b } : a ?? b;
-}
