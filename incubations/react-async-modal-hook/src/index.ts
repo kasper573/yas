15ed7d4feaa5e2b05ae3794c7e82091f34252935
ksandin <kasper.sandin@gmail.com>
2024-06-09 09:34:02 +0200

    chore: remove react-async-modal-hook

--- incubations/react-async-modal-hook/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./ModalOutlet";
-export * from "./ModalContext";
-export * from "./ModalStore";
-export * from "./useModals";
-export * from "./useModal";
-export * from "./useModalSustainer";
-export type * from "./constants";
-export type * from "./deferPromise";
