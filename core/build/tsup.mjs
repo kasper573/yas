b3257c82519466f7b6b49881ceee674fe38d7f1b
ksandin <kasper.sandin@gmail.com>
2024-06-16 09:39:49 +0200

    fix: allow shared tsup config to be given extra options

--- core/build/tsup.mjs
+++ core/build/tsup.mjs
@@ -1,22 +1,14 @@
 import * as fs from "fs";
 import { defineConfig as defineTsupConfig } from "tsup";
-/** @import { Options } from "tsup" */
 
 /**
- * @typedef YasTsupConfig
- * @type  {object}
- * @property {Exclude<Options["entry"], undefined>} entry
- */
-
-/**
- * @param {YasTsupConfig} options
+ * @param {import("tsup").Options} options
  */
 export function defineConfig(options) {
-  const { entry } = options;
   return defineTsupConfig({
-    entry,
     format: ["cjs", "esm"],
     clean: true,
     dts: true,
+    ...options
   });
 }
