5c57f5869d426f8173fc76d840c670c83206961e
ksandin <kasper.sandin@gmail.com>
2024-06-16 08:02:32 +0200

    chore: remove deprecated build settings

--- core/build/tsup.mjs
+++ core/build/tsup.mjs
@@ -1,66 +1,22 @@
-import * as path from "path";
 import * as fs from "fs";
-import { defineConfig } from "tsup";
-import { defineEnv } from "./defineEnv.mjs";
+import { defineConfig as defineTsupConfig } from "tsup";
+/** @import { Options } from "tsup" */
 
 /**
- * @param {string} projectRoot
- * @param {Partial<import("tsup").Options>} options
+ * @typedef YasTsupConfig
+ * @type  {object}
+ * @property {Exclude<Options["entry"], undefined>} entry
  */
-export function createYasTsupConfig(projectRoot = process.cwd(), options = {}) {
-  const { internalPackages, entry } = analyzePackage(projectRoot);
-  return defineConfig({
+
+/**
+ * @param {YasTsupConfig} options
+ */
+export function defineConfig(options) {
+  const { entry } = options;
+  return defineTsupConfig({
+    entry,
     format: ["cjs", "esm"],
     clean: true,
     dts: true,
-    noExternal: internalPackages,
-    define: defineEnv(projectRoot),
-    esbuildOptions(options) {
-      options.logOverride = {
-        // We sometimes intentionally define NODE_ENV as undefined to allow the runtime to fall back to a default.
-        // esbuild has a built-in warning to help people to avoid this as it's usually a mistake. We're not making a mistake.
-        "suspicious-define": "silent",
-      };
-    },
-    entry,
-    ...options,
   });
-}
-
-/**
- * @param {string} projectRoot
- * @returns {{internalPackages: string[], entry: { [bundleName: string]: string }}
- */
-function analyzePackage(projectRoot) {
-  const packageJson = JSON.parse(
-    fs.readFileSync(`${projectRoot}/package.json`, "utf-8"),
-  );
-
-  // Derive internal packages from dependency fields
-  const internalPackages = [];
-  const { dependencies, devDependencies, peerDependencies } = packageJson;
-  for (const deps of [dependencies, devDependencies, peerDependencies]) {
-    for (const [packageName, packageVersion] of Object.entries(deps ?? {})) {
-      if (
-        packageVersion.startsWith("workspace:") &&
-        !internalPackages.includes(packageName)
-      ) {
-        internalPackages.push(packageName);
-      }
-    }
-  }
-
-  // Derive entry points from exports field.
-  const entry = {};
-  const { exports = {} } = packageJson;
-  for (const [exportKey, exportPaths] of Object.entries(exports)) {
-    const bundleName =
-      exportKey === "." ? "index" : exportKey.replace(/^\.\//, "");
-    entry[bundleName] = path.resolve(
-      projectRoot,
-      exportPaths.import ?? exportPaths.require,
-    );
-  }
-
-  return { internalPackages, entry };
 }
