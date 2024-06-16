4525a1542672109f6af106e0731de805fd538362
ksandin <kasper.sandin@gmail.com>
2024-06-16 04:21:46 +0200

    fix: lower cutoff for the constrastColor function

--- libraries/ui/src/devtools/Palette.tsx
+++ libraries/ui/src/devtools/Palette.tsx
@@ -64,5 +64,5 @@ export function contrastColor(color: string) {
   }
 
   const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-  return luminance > 0.5 ? "#000" : "#fff";
+  return luminance > 0.4 ? "#000" : "#fff";
 }
