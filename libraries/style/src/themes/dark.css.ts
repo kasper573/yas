9c181d8182d539e4e5a1f622aa9f242cfa71f576
Kasper <805006+kasper573@users.noreply.github.com>
2024-06-30 22:42:22 +0200

    fix: prebuild style package

--- libraries/style/src/themes/dark.css.ts
+++ libraries/style/src/themes/dark.css.ts
@@ -1,5 +1,5 @@
 import { createTheme } from "@vanilla-extract/css";
 import * as darkValues from "@yas/design-tokens/themes/dark";
-import { theme } from "../src/theme.css";
+import { theme } from "../theme.css";
 
 export const dark: string = createTheme(theme, darkValues);
--- libraries/style/src/themes/light.css.ts
+++ libraries/style/src/themes/light.css.ts
@@ -1,5 +1,5 @@
 import { createTheme } from "@vanilla-extract/css";
 import * as lightValues from "@yas/design-tokens/themes/light";
-import { theme } from "../src/theme.css";
+import { theme } from "../theme.css";
 
 export const light: string = createTheme(theme, lightValues);
