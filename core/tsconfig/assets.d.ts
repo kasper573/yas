6f907417dd39f9c2e323823908533b58667868c8
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-02 19:55:39 +0200

    fix: remove assets.d.ts

--- core/tsconfig/assets.d.ts
+++
@@ -1,7 +0,0 @@
-// Maintenance note: Make sure this file always represents
-// the same asset types as the ones supported by core/build.
-
-// The reason we have this file in core/tsconfig and not core/build
-// is so that react library packages only have to depend on core/tsconfig to get valid types.
-
-import "vite/client";
